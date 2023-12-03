549f4d2b76b0a4efd86a2a83938229181aa0f85f
romersonrsantana <romersonsantana@hotmail.com.br>
2023-12-03 05:51:54 -0300

    Create ex002.py

---
+++ ex002.py
@@ -0,0 +1,5 @@
+mensagem = ('Hello, World !!')
+nome = input ('Qual é o seu nome?')
+print('Olá ' + nome + ' !! Não perca mais tempo, dê o seu primeiro: ' + mensagem + '!!')
+
+print('Olá ' + nome + ' !! Prazer em te conhecer!') 
