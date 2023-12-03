9a973ac7e85a45327e00253fccb37455cc31e5a0
romersonrsantana <romersonsantana@hotmail.com.br>
2023-12-03 18:29:07 -0300

    Te amo Deus

---
+++ desafios/des001.py
@@ -0,0 +1,4 @@
+idade = input('Qual é a sua idade?')
+mes = input('Em qual mês você nasceu?')
+ano = input('De qual ano?')
+print('Você tem', idade , 'anos. Pois nasceu no mês de' , mes,'no ano de',ano , '.Correto?')
---
+++ desafios/des002.py
@@ -0,0 +1,4 @@
+valor1 = input('Digite um número: ')
+valor2 = input('Digite un outro número: ')
+soma = (valor1 + valor2)
+print('O resultado do ', valor1, 'somado ao ', valor2, 'é igual a ', soma)
