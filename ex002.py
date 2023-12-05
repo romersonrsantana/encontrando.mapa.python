2fa4b9be7a5d29c7f5aa263fd01459cba6f4d489
romersonrsantana <romersonsantana@hotmail.com.br>
2023-12-04 22:49:24 -0300

    Voltando para a trilha correta
    Louvado Seja Nosso Senhor Jesus Cristo. Para Sempre Seja Louvado. Amém.

--- ex002.py
+++ ex002.py
@@ -1,5 +1,5 @@
 mensagem = ('Hello, World !!')
-nome = input ('Qual é o seu nome?')
+nome = input('Qual é o seu nome?')
 print('Olá ' + nome + ' !! Não perca mais tempo, dê o seu primeiro: ' + mensagem + '!!')
 
-print('Olá ' + nome + ' !! Prazer em te conhecer!') 
+print('Olá ' + nome + ' !! Prazer em te conhecer!')
