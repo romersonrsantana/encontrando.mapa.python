e0b5e602e7c34208bb0c189fe254b74e385a19a1
romersonrsantana <romersonsantana@hotmail.com.br>
2023-12-06 21:54:05 -0300

    no caminho
    de volta ao caminho,
    Louvado Seja Nosso Senhor Jesus Cristo, Para Sempre seja Louvado. Amém!!

--- teste/test001.py
+++ teste/test001.py
@@ -2,4 +2,4 @@ n1 = int(input('Digite um número: '))
 n2 = int(input('Digite um outro número: '))
 soma = n1 + n2
 #print('A soma dos valores entre', n1, 'e', n2 , 'é' , soma)
-print('A soma dos valore {} e do valor {} corresponde {} .'.format(n1, n2, soma))
+print('A soma dos valore {} e do valor {} corresponde {}'.format(n1, n2, soma))
---
+++ teste/teste002.py
@@ -0,0 +1,6 @@
+n1 = input('Digite um número: ')
+print('O número escolhido possui como caracteristica ' , type(n1))
+n2 = int(input('Digite um outro número: '))
+print('O valor escolhido possui como caracteristica ' , type(n2))
+soma = int(n1) + n2
+print('A soma dos valores {} e {}, correponde ao total de {}'.format(n1, n2, soma))
