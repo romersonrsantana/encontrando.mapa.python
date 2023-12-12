f5f7b0fa4706c6fd6638b89b6e5138ed6715013f
romersonrsantana <romersonsantana@hotmail.com.br>
2023-12-12 05:47:03 -0300

    Encontrando a trilha certa
    Louvado Seja Nosso Senhor Jesus Cristo. Para sempre Seja Louvado. Amém.

--- desafios/des009.py
+++ desafios/des009.py
@@ -1,1 +1,3 @@
-n1
+print('\n Seja bem vindo!! \n\n Aqui você pode escolher um número inteiro que iremos montar a tabuada dele pra você!! ')
+n1 = int(input('\n Digite um número: '))
+print('\n O número informado foi {} , \n sua tabuada será: \n {} x 0 = {} \n {} x 1 = {} \n {} x 2 = {} \n {} x 3 = {} \n '.format(n1, n1, (n1*0), n1, (n1*1), n1, (n1*2), n1, (n1*3) ))
