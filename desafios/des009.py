0a560700f2cdd16bc62383c5e263fdc8d1394383
romersonrsantana <romersonsantana@hotmail.com.br>
2023-12-12 22:17:37 -0300

    Update des009.py

--- desafios/des009.py
+++ desafios/des009.py
@@ -1,3 +1,6 @@
-print('\n Seja bem vindo!! \n\n Aqui você pode escolher um número inteiro que iremos montar a tabuada dele pra você!! ')
+nome = input('Digite seu nome: ')
+print('\n Seja bem vindo(a) {} !! \n\n Aqui você pode escolher um número inteiro que iremos montar a tabuada dele pra você!! ' .format(nome))
 n1 = int(input('\n Digite um número: '))
-print('\n O número informado foi {} , \n sua tabuada será: \n {} x 0 = {} \n {} x 1 = {} \n {} x 2 = {} \n {} x 3 = {} \n '.format(n1, n1, (n1*0), n1, (n1*1), n1, (n1*2), n1, (n1*3) ))
+print('\n O número informado foi {} , \n sua tabuada será: \n\n {} x 0 = {} \n {} x 1 = {} \n {} x 2 = {} \n {} x 3 = {} \n {} x 4 = {} \n {} x 5 = {} \n {} x 6 = {} \n {} x 7 = {} \n {} x 8 = {} \n {} x 9 = {} \n {} x 10 = {} \n\n '.format(n1, n1, (n1*0), n1, (n1*1), n1, (n1*2), n1, (n1*3), n1, (n1*4), n1, (n1*5), n1, (n1*6), n1, (n1*7), n1, (n1*8), n1, (n1*9), n1, (n1*10)))
+
+print('Obrigado por participar {} !!! \n\n' .format(nome))
