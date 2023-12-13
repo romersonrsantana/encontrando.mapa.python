78a00eae9e2fbf7be4eba30c2d5e7e38f8850684
romersonrsantana <romersonsantana@hotmail.com.br>
2023-12-13 05:44:02 -0300

    Mais uma etapa do percurso
    Louvado Seja Nosso Senhor Jesus Cristo. Para Sempre Seja Louvado. Amém.

--- desafios/des010.py
+++ desafios/des010.py
@@ -1,1 +1,7 @@
-real = float(input('Bem vindo ao conversor de moedas'))
+constante = 1/4.97
+
+nome = input('\n\n Digite seu nome para iniciarmos: ')
+
+real = float(input('\n\n Bem vindo a carteira digital {} ! Aqui você pode definir quanto de dólar deseja comprar. \n\n Insira seu valor de compra: ' .format(nome)))
+
+print(' \n Com R$ {:.2f} reais você pode comprar U$ {:.2f} dolares! \n\n Obrigado por ter escolhido nossa carteira digital {} !! \n\n' .format(real, (real*constante), nome))
