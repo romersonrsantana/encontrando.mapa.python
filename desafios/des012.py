7b13bb1c268d384a2ab4614136644f9c358c709b
romersonrsantana <romersonsantana@hotmail.com.br>
2023-12-14 05:52:16 -0300

    Entendendo o percurso
    Louvado Seja Nosso Senhor Jesus Cristo. Para Sempre Seja Louvado. Amém.

--- desafios/des012.py
+++ desafios/des012.py
@@ -1,6 +1,11 @@
 mensagem = str('Calculo de desconto')
 print('\n\n {:-^63} \n\n' .format(mensagem))
-print(float(input('Digite o valor do produto para o desconto: \n')))
-print(int(input('Insira quanto de desconto você deseja aplicar \n (valores permitidos de 1 a 100%): ')))
-print('O valor informado para o desconto foi de R$ {} o desconto derá de {} %. \n\n Caro cliente o desconto será de R$ {} reais. \n Seu produto sairá por R$ {} reais' )
 
+precoproduto = float(input('Digite o valor do produto para o desconto: '))
+
+descontoporcento = float(input('Insira quanto de desconto você deseja aplicar \n (valores permitidos de 1 a 100%): '))
+
+desconto = (precoproduto * (descontoporcento / 100))
+valorfinal = (precoproduto - desconto)
+
+print('O valor informado para o desconto foi de R$ {} o desconto será de {} %. \n\n Caro cliente o desconto será de R$ {} reais. \n Seu produto sairá por R$ {} reais' .format(precoproduto, descontoporcento, desconto, valorfinal ))
