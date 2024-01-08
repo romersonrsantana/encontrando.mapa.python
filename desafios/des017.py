4134658cdf426ddc82911d7ebdb34ca31e486ef4
romersonrsantana <romersonsantana@hotmail.com.br>
2024-01-08 19:47:35 -0300

    Reencontrando o caminho
    O Senhor é o caminho, a verdade e a luz Meu Senhor Jesus Cristo. Ninguém vai ao Pai se não for através de Ti. Louvado Seja Nosso Senhor Jesus Cristo. Para Sempre Seja Louvado. Amém.

--- desafios/des017.py
+++ desafios/des017.py
@@ -2,4 +2,4 @@ import math
 
 b = int(input('\n Digite o valor do cateto oposto que iremos chamar de b: '))
 c = int(input('\n Digite o valor do cateto adjacente que iremos chamar de c: '))
-print('\n Através dos valores digitados {} e {}, iremos calcular o valor da hipotenusa. A hipotenusa será {}'.format(b, c, (pow(b) + pow(c)))
+print('\n Através dos valores digitados {} e {}, iremos calcular o valor da hipotenusa. \n\n A hipotenusa será: {} \n\n'.format(b, c, (b**2)+(c**2)))
