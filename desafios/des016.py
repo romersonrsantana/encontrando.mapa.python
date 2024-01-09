757d362242077d13f99554ea3bae7276f79db48c
romersonrsantana <romersonsantana@hotmail.com.br>
2024-01-08 22:10:52 -0300

    percurso
    Louvado Seja Nosso Senhor Jesus Cristo. Para sempre Seja Louvado. Amém.

--- desafios/des016.py
+++ desafios/des016.py
@@ -1,2 +1,6 @@
 numero = float(input('Digite um número: '))
-print('O número que você digitou foi {}'.format(int(numero)))
+print('O número que você digitou foi {} e a sua porção inteira é {}\n\n'.format(float(numero), int(numero)))
+
+'''resolução guanabara math.trunc(num)
+from math import trunc ---> trunc(num)'''
+
--- desafios/des017.py
+++ desafios/des017.py
@@ -1,5 +1,10 @@
 import math
 
-b = int(input('\n Digite o valor do cateto oposto que iremos chamar de b: '))
-c = int(input('\n Digite o valor do cateto adjacente que iremos chamar de c: '))
-print('\n Através dos valores digitados {} e {}, iremos calcular o valor da hipotenusa. \n\n A hipotenusa será: {} \n\n'.format(b, c, (b**2)+(c**2)))
+b = float(input('\n Digite o valor do cateto oposto que iremos chamar de b: '))
+c = float(input('\n Digite o valor do cateto adjacente que iremos chamar de c: '))
+hip = ((b ** 2 + c ** 2) ** (1/2))
+print('\n Através dos valores digitados {} e {}, iremos calcular o valor da hipotenusa. \n\n A hipotenusa será: {:.2f} \n\n'.format(b, c, hip))
+
+'''Resolução guanabara import math
+hip = math.hypot(b, c)
+'''
