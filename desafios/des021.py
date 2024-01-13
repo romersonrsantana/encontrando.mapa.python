7ff2e4846bebe35f56d0282ac338f0cec7cdd4d1
romersonrsantana <romersonsantana@hotmail.com.br>
2024-01-12 23:21:32 -0300

    Um passo de cada vez
    Louvado Seja Nosso Senhor Jesus Cristo, para Sempre Seja Louvado. Amém.

---
+++ desafios/des021.py
@@ -0,0 +1,7 @@
+import pygame
+pygame.init()
+pygame.mixer.music.load()
+pygame.mixer.music.play()
+pygame.event.wait()
+
+'''Carrega o o arquivo, após executar o programa termina quando a musica chega ao fim'''
