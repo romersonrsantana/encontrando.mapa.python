be3433715540a8b9e3228c322c81a0200cc9af36
romersonrsantana <romersonsantana@hotmail.com.br>
2024-01-17 22:13:56 -0300

    Te Amo Deus

---
+++ desafios/des025.py
@@ -0,0 +1,3 @@
+print('\n ------- Identificador de silva ------')
+nome = str(input('\n\n Qual seu nome e sobrenome? '))
+print('\n\n Analisando o nome informado \n\n Correspodência para o sobrenome SILVA : {} \n\n'.format('SILVA' in nome.upper()))
---
+++ desafios/des026.py
@@ -0,0 +1,3 @@
+print(' \n\n ------ Avaliando a frase ------- ')
+frase = str(input('\n\n Escreva uma frase: '))
+print('\n\n A frase escolhida tem: \n\n {} letra(s) \n\n ' .format(frase.upper().count(A)))
