669d56c748204f9457e29d5473aaf9bb31d81abc
romersonrsantana <romersonsantana@hotmail.com.br>
2024-01-21 21:40:41 -0300

    Update des026.py

--- desafios/des026.py
+++ desafios/des026.py
@@ -1,3 +1,3 @@
 print(' \n\n ------ Avaliando a frase ------- ')
-frase = str(input('\n\n Escreva uma frase: '))
-print('\n\n A frase escolhida tem:\n\n {} caracteres \n\n {} letra(s) --> a \n\n Ela aparece pela primeira vez na posição --> {} \n\n' .format(len(frase),frase.upper().count('A'), frase.upper().find('A')))
+frase = str(input('\n\n Escreva uma frase: ')).upper()
+print('\n\n A frase escolhida tem:\n\n {} caracteres \n\n {} letra(s) --> a \n\n Ela aparece pela primeira vez na posição --> {} \n\n A última letra aparece {}' .format(len(frase),frase.upper().count('A'), frase.find('A')+1, frase.rfind('A')+1))
