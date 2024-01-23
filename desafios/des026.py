aa92f30edf3e7d5af4ac027724530d5a53267d1e
romersonrsantana <romersonsantana@hotmail.com.br>
2024-01-22 21:37:35 -0300

    Um passo de cada vez
    Louvado Seja Nosso Senhor Jesus Cristo. Para Sempre Seja Louvado. Amém.

--- desafios/des026.py
+++ desafios/des026.py
@@ -1,3 +1,4 @@
 print(' \n\n ------ Avaliando a frase ------- ')
-frase = str(input('\n\n Escreva uma frase: ')).upper()
-print('\n\n A frase escolhida tem:\n\n {} caracteres \n\n {} letra(s) --> a \n\n Ela aparece pela primeira vez na posição --> {} \n\n A última letra aparece {}' .format(len(frase),frase.upper().count('A'), frase.find('A')+1, frase.rfind('A')+1))
+frase = str(input('\n\n Escreva uma frase: ')).upper().strip()
+print('\n\n A frase escolhida tem:\n\n {} caracteres \n\n {} letra(s) --> a \n\n Ela aparece pela primeira vez na posição --> {} \n\n A última letra aparece {}' .format(len(frase),frase.upper().count('A'), frase.find('A'), frase.rfind('A')))
+print('\n\n A escala considerada começa em 0 \n\n')
--- desafios/des027.py
+++ desafios/des027.py
@@ -1,3 +1,3 @@
-nome = str(input('\n\n Digite seu nome e sobrenome: '))
+nome = str(input('\n\n Digite seu nome e sobrenome: ')).strip()
 divisao = nome.split()
-print('\n\n O nome digitado foi: {} \n\n Seu primeiro nome é: {} \n\n Seu sobrenome: {}. \n\n Seu nome possui um total de {} caracteres.\n\n'.format(nome, divisao[0], divisao[1:], len(divisao)))
+print('\n\n O nome digitado foi: {} \n\n Seu primeiro nome é: {} \n\n Seu último nome é: {}. \n\n Seu nome possui um total de {} caracteres.\n\n'.format(nome, divisao[0], divisao[len(divisao)-1], len(divisao[0])))
