86a7da8ddad69d40d9d46013087be5c1f612dfc1
romersonrsantana <romersonsantana@hotmail.com.br>
2024-01-26 23:45:42 -0300

    desvendando o mapa
    Louvado Seja Nosso Senhor Jesus Cristo. Para Sempre Seja Louvado. Amém.

---
+++ desafios/des028.py
@@ -0,0 +1,6 @@
+import random
+print('\n\n ------ Adivinhando um número --------')
+numb = print('\n\n Escolha um número de 0 a 5:')
+seqNum = [0, 1, 2, 3, 4, 5]
+escolhido = random.choice(numb)
+print('O número esdolhido foi {}'.format(numb))
