77f874b5afd958d09ca445a7a082ce17d9c82641
romersonrsantana <romersonsantana@hotmail.com.br>
2024-02-01 04:39:06 -0300

    Um passo a mais
    Louvado Seja Nosso Senhor Jesus Cristo. Para Sempre Seja Louvado. Amém.

--- desafios/des035.py
+++ desafios/des035.py
@@ -1,6 +1,8 @@
 print('\n\n Condição de existência: --- triangulo ---- \n\n informe o tamanho de 3 retas e descubra se elas podem formar um triângulo...\n\n')
 reta1 = float(input('Informe o tamanho da reta 01: '))
-reta2 = float(input('Informe o tamanho da reta2: '))
-reta3 = float(input('Inorme o tamanho da reta 03: '))
-if reta2 - reta3 < reta1 and reta2 + reta3 > reta3:
-    
+reta2 = float(input('\n\n Informe o tamanho da reta2: '))
+reta3 = float(input('\n\n Informe o tamanho da reta 03: '))
+if (reta2 + reta1) > reta3 and (reta1 + reta3) > reta2 and (reta2 + reta3) > reta1:
+    print('\n\n Com essas retas é possivel formar um triangulo.\n\n')
+else:
+    print('\n\n Não é possivel formar um triângulo!!\n\n')
