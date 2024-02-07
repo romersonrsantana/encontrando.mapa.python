a74dc5732efc6a9cebbbbc214e6d8ba992822b50
romersonrsantana <romersonsantana@hotmail.com.br>
2024-02-07 19:27:39 -0300

    de passo em passp
    Louvado Seja Nosso Senhor Jesus Cristo. Para Sempre Seja Louvado. Amém.

--- desafios/des032.py
+++ desafios/des032.py
@@ -7,7 +7,7 @@ if ano == 0:
 if ano < 1582:
     print('\n\n [ERRO!!] O ano informado não é valido!!\n\n Considerando o cal1endário gregoriano o ano mínimo permitido é 1582, quando passou a vigorar. \n\n')
 else:
-    if (ano% 4) == 0 and (ano%100) != 0 or ano % 400 == 0:
+    if (ano%4) == 0 and (ano%100) != 0 or ano % 400 == 0:
         print('\n\n O ano informado é BISSEXTO!! \n\n Nele {}, você terá um dia a mais para lutar pelos seus sonhos!!\n\n Louvado Seja Nosso Senhor Jesus Cristo.\n Para Sempre Seja Louvado. Amém.\n\n'.format(nome))
     else:
         print('\n\n Não. {} NÃO É UM ANO BISSEXTO!! \n\n'.format(ano))
