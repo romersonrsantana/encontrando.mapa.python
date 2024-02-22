10d54b6e3b2b5a0a36c835626433fc3d59175243
romersonrsantana <romersonsantana@hotmail.com.br>
2024-02-22 16:41:35 -0300

    De passo em passo
    Louvado Seja Nosso Senhor Jesus Cristo. Para Sempre Seja Louvado. Amém.

--- desafios/cores.desafios/cor009.py
+++ desafios/cores.desafios/cor009.py
@@ -3,4 +3,8 @@ cores = {'azul':'\033[44m',
         'limpa':'\033[m'}
 num = int(input('\n\n' + '\033[1;32;42m' + '- = '*6 + 'Para começarmos a tabuada' + ' - = '*6 + '\033[m' + '\n\nDigite um número inteiro: '))
 print(6*'{}  '.format(cores['azul']))
-print('{}{} x {} = {}'.format(cores['limpa'],num, 1, num*1))
+print('{}{} x {} = {}'.format(cores['limpa'],num, 0, num*0))
+print('{} x {} = {}'.format(num, 1, num*1))
+print('{} x {} = {}'.format(num, 2, num*2))
+print('{} x {} = {}'.format(num, 3, num*3))
+print('{} x {} = {}'.format(num, 4, ))
