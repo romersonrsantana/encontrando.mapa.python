7efe834104087b0a0475f9c447b034b3eda6dfa8
romersonrsantana <romersonsantana@hotmail.com.br>
2024-03-14 20:31:37 -0300

    De passo em passo
    Louvado Seja Nosso Senhor Jesus. Para Sempre Seja Louvado. Amém.

---
+++ desafios/algoritmos.logicaprogramacao/ex001.py
@@ -0,0 +1,16 @@
+print('Analisador de moedas')
+#O peso das moedas foram dividos,  eno resultado de cada moeda será colocada dentro em uma variável,
+
+#calculo
+print('Considerando os valores que estão no cumputador')
+PM1 = 5 
+PM2 = 5
+PM3 = 5
+PM4 = 9
+PM5 = 5
+
+soma1 = PM1 + PM2
+soma2 = PM3 + PM5
+
+if PM1 == PM3:
+    print('A Moeda falsa está na variavel PM4 ')
