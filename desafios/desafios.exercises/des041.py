d1b3d4194820fcab24c407ffe4270a51cb3d176f
romersonrsantana <romersonsantana@hotmail.com.br>
2024-03-26 22:49:27 -0300

    De passo em passo
    Toda honra pra Ti, Deus de Abrão, Isaac e Jacó. Louvado Seja Nosso Senhor Jesus Cristo. Para sempre seja Louvado.

--- desafios/desafios.exercises/des041.py
+++ desafios/desafios.exercises/des041.py
@@ -1,7 +1,29 @@
+from datetime import date
 #colors palete
 
 print(''' \nNational Swimming Confederation''',
       '''\nKnowing your category''','''\nBut...\n''')
 
+currentyear = date.today().year
+
 #User data
 name = str(input('''what's, your name? '''))
+print('\nNice to meet you, {}!!'.format(name))
+years = int(input('\nEnter your year of birth whith four number: '))
+#calculation
+age = currentyear - years
+
+#decision structure
+if years > currentyear or years < 1924:
+    print('\n[ERRO] Rewrite the values with the correct year!\n')
+else:
+      if age >= 0 and age <= 9:
+            print('\n Your category is child!\n')
+      elif age > 9 and age <= 14:
+           print('\n Your category is children!\n')
+      elif age > 14 and age <= 19:
+           print('\n Your category is junior!\n')
+      elif age > 19 and age <= 40:
+           print('\n Your category is senior!\n')
+      else:
+           print('\n Your category is master!\n')
