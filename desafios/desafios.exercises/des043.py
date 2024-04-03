efa918adb1553f221d0da57b552d03a2a59d6c47
romersonrsantana <romersonsantana@hotmail.com.br>
2024-04-03 04:50:22 -0300

    de passo em passo
    Toda Glória pra Ti Deus de Abraão, Isaac e Jacó. Louvado Seja Nosso Senhor Jesus Cristo. Para Sempre Seja Louvado. Amém.

--- desafios/desafios.exercises/des043.py
+++ desafios/desafios.exercises/des043.py
@@ -1,8 +1,15 @@
-print('\n',20*' ','Body Mass Index\n\n','Enter your details below')
+print('\n',20*' ','Body Mass Index\n\n','Enter your details below\n\n')
 weight = float(input('Tell us your weight: ---> '))
-if weight <= 17 or weight > 595:
-    print('The data provided is outside the established standard.\nRewrite the value.\n')
+
+if weight <= 17 or weight >= 512:
+    print('\nThe data provided is outside the established standard.\nRewrite the value.\n')
 else:
-    height = float(input('Tell us your heigth'))
-    if height <= 1.00 or height > 2.50:
-        print('The date provided is outside the established,~LNLN standard. \n\n ')
+    height = float(input('Tell us your heigth: ---> '))
+    if height <= 0.70 or height > 4.50:
+        print('\nThe date provided is outside the established standard.\nRewrite the value\n')
+    else:
+        BodyMass = weight/(height**2)
+        if BodyMass < 18.5:
+            print("\nYour body mass index {:.2f}\nYou're underweight".format(BodyMass))
+        elif BodyMass >= 18.5 and BodyMass <= 25:
+            print('')
