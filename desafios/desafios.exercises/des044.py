f28d46fc2870ce1e99aa022ea7edc5ffc7893655
romersonrsantana <romersonsantana@hotmail.com.br>
2024-05-03 00:18:22 -0300

    De passo em passo
    Toda Honra e Glória ao Deus de Abraão, Isaac e Jacó. Louvado Seja Nosso Senhor Jesus Cristo. Para Sempre Seja Louvado. Amém.

--- desafios/desafios.exercises/des044.py
+++ desafios/desafios.exercises/des044.py
@@ -14,4 +14,19 @@ else:
     if choice < 1 or choice > 4:
         print('\n [ERRO] Enter a valid number \n')
     else:
+        if choice == 1:
+            print('\n With this payment method you will have a 20\%\ discount. \n\n The product it costs U$ {} \n\n In this form of payment, the discount will be U$ {} \n\n Amount {}'.format(value, (value*0.20), value-(value*0.2)))
+        elif choice == 2:
+            print('\n With this payment method you will have a 5\\%\\ discount. \n\n The product it costs U$ {}. \n\n In this form of payment, the discount will de U$ {}.\n\n Amount {}'.format(value, (value*0.5), value-(value*0.5)))
+        elif choice == 3:
+            installments = int(input('\n You want to divid the amount in 2x? \n\n 1-yes \n\n 2-no. Only 1x on credit card.'))
+            if installments < 1 or installments > 2:
+                print('[ERRO] Write a valid value!')
+            elif installments == 1:
+                print('\nYou will not receive discount with this payment method.\n\n The product it costs U$ {}. \n')
+            elif installments == 2:
+                print('\nYou will not receive discount with this payment method.\n\n The product it costs U$ {}.\n\n In 2x installments you will pay a total of {}\n'.format(value,value/2))
+        elif choice == 4:
+            print('You can split it up to 10x with 20\\%\\ interest!\n')
+            
         
