7584bb2545c69b890484bef33486f16b6197124f
romersonrsantana <romersonsantana@hotmail.com.br>
2024-05-31 17:38:02 -0300

    De passo em passo
    Toda Honra e Toda Glória ao Deus Vivo, Ao Deus de Israel, Ao Deus de Abraão, Isaac, Jacó e Moises. Louvado Seja Nosso Senhor Jesus Cristo. Para Sempre Seja Louvado. Amém.

--- desafios/desafios.exercises/des045.py
+++ desafios/desafios.exercises/des045.py
@@ -11,22 +11,25 @@ escolha = int(input('''Escolha um número:
                     2 - Tesoura ✌
                     
                     ---> ''' ))
-print(' Processando...')
-sleep(3)
-if escolha == num:
-    print('\n Empatou!! \n')
-elif escolha < num != 1:
-    print('\n Você ganhou!!\n')
-elif escolha > num != 2:
-    print('\n Você ganhou!!\n')
-elif escolha > num != 1:
-    print('\n Você perdeu!!\n')
-elif escolha < num != 0:
-    print('\n Você perdeu!!\n')
-print('')
-print("Você escolheu ", example_list[escolha])
-print('')
-print("O computador escolheu ", example_list[num])
-print('')
+if escolha >= 0 and escolha < 3:
+    print('Carregando...')
+    sleep(3)
+    if escolha == num:
+        print('\nEmpatou!!\n')
+    elif escolha < num != 1:
+        print('\nVocê ganhou!!\n')
+    elif escolha > num != 2:
+        print('\nVocê ganhou!!\n')
+    elif escolha > num != 1:
+        print('\nVocê perdeu!!\n')
+    elif escolha < num != 0:
+        print('\nVocê perdeu!!\n')
+    print('')
+    print("Você escolheu ", example_list[escolha])
+    print('')
+    print("O computador escolheu ", example_list[num])
+    print('')
+else:
+    print('\nTente uma opção válida!\n')
 #Toda Honra e Toda Glória ao Deus de Israel, Ao Deus de Abraão, Isaac, Jacó e Moises. Louvado Seja Nosso Senhor Jesus Cristo. Para Sempre Seja Louvado. Amém.
 
