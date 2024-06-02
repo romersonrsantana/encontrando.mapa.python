aac8625dd53cbaf21b558bdaa6a5e7e784676568
romersonrsantana <romersonsantana@hotmail.com.br>
2024-06-02 13:37:25 -0300

    De passo em passo
    Toda Honra e Toda Glória Ao Deus Vivo, Ao Deus de Israel, Ao Deus de Abraão, Isaac, Jacó e Moisés. Louvado Seja Nosso Senhor Jesus Cristo. Para Sempre Seja Louvado. Amém.

---
+++ desafios/pit.stop-guanabara/pit.stop005.jog.005.py
@@ -0,0 +1,49 @@
+from random import randint
+from time import sleep
+#criar uma variável usando ().
+itens = ('Pedra','Papel','Tesoura')
+computador = randint(0,2)
+jogador = int(input('''Escolha um número:
+                  [0] Pedra
+                  [1] Papel
+                  [2] Tesoura
+                    ---> '''))
+if jogador >= 0 and jogador <= 2:
+    print('JO')
+    sleep(1)
+    print('KEN')
+    sleep(1)
+    print('PÔ')
+    print('')
+    print('=-='*11)
+    print('O computador escolheu {}'.format(itens[computador]))
+    print('\nVocê escolheu {}'.format(itens[jogador]))
+    print('=-='*11)
+    #print('\nO computador escolheu {}\n'.format(itens[computador]))
+    if computador == 0:
+        if jogador == 0:
+            print('\n Empatou!! \n')
+        elif jogador == 1:
+            print('\nVocê venceu!!\n')
+        elif jogador == 2:
+            print('\nVocê perdeu!!\n')
+
+    elif computador == 1:
+        if jogador == 0:
+            print('\nVocê perdeu!!\n')
+        elif jogador == 1:
+            print('\nEmpatou!!\n')
+        elif jogador == 2:
+            print('\nVocê ganhou!!\n')
+        
+    elif computador == 2:
+        if jogador == 0:
+            print('\nVocê ganhou!!\n')
+        elif jogador == 1:
+            print('\nvocê perdeu!!\n')
+        elif jogador == 2:
+            print('\nEmpatou!!\n')
+            
+else:
+    print('\nInforme uma opção correta\n')
+
