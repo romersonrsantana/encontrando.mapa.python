4be2f536ce56af71be36676213c02976e3530eab
romersonrsantana <romersonsantana@hotmail.com.br>
2024-06-06 18:53:37 -0300

    De passo em passo
    Toda Honra e Toda Glória Ao Deus Vivo, Ao Deus de Israel, Ao Deus de Abraão, Isaac, Jacó e Moises. Louvado Seja Nosso Senhor Jesus Cristo. Para Sempre Seja Louvado. Amém.

---
+++ teste/teste004.py
@@ -0,0 +1,7 @@
+#colors palet
+colors = {'blue':'\033[1;34m',
+          'purple':'\033[1;35m',
+          'green':'\033[1;32m',
+          'yellow':'\033[1;33m',
+          'clean':'\033[m'}
+print('countdown - test')
