48c9e899bed27937b64aed6f8b786533a31391b5
romersonrsantana <romersonsantana@hotmail.com.br>
2024-06-06 18:51:27 -0300

    De passo em passo
    Toda Honra e Toda Glória Ao Deus Vivo, Ao Deus de Israel, Ao Deus de Abraão, Ao Deus de Isaac, Jacó e Moises. Louvado Seja Nosso Senhor Jesus Cristo. Para Sempre Seja Louvado. Amém.

--- desafios/desafios.exercises/des046.py
+++ desafios/desafios.exercises/des046.py
@@ -1,15 +1,19 @@
+import emoji
 from time import sleep
-#colors palet
-colors = {'blue':'\033[1;34m',
-          'purple':'\033[1;35m',
-          'purple-title':'\033[1;39;45m',
+#colors palete
+colors = {'blue':'\033[1;39;44m',
+          'purple':'\033[1;39;45m',
           'gray':'\033[1;37m',
           'clean':'\033[m'}
 
-print(' '*27,'{} Contagem Regressiva {}'.format(colors['purple-title'],colors['clean']))
+print(' '*27,'{} Contagem Regressiva {}'.format(colors['purple'],colors['clean']))
 print()
+sleep(1.6)
+print(' '*27,'{}       Ready...  🧨  {}'.format(colors['blue'],colors['clean']))
 for c in range(10, 0-1, -1):
     sleep(1.3)
     print()
-    print(c)
-    
+    print('{}--->{} '.format(colors['gray'],colors['clean']),c)
+    sleep(1.6)
+print()
+print(emoji.emojize("💥💥💥💥💥💥💥💥💥💥💥\n💥💥💥💥💥💥💥💥💥💥💥\n💥💥💥💥💥💥💥💥💥💥💥💥💥\n"))
