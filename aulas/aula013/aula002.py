cbe08b7f65225b790394fcb9b14e98212fe112d6
romersonrsantana <romersonsantana@hotmail.com.br>
2024-06-05 22:10:36 -0300

    De passo em passo
    Toda Honra e Toda Glória ao Deus de Vivo, Ao Deus de Israel, Ao Deus de Abraão, Isaac, Jacó e Moises. Louvado Seja Nosso Senhor Jesus Cristo. Para Sempre Seja Louvado. Amém.

--- aulas/aula013/aula002.py
+++ aulas/aula013/aula002.py
@@ -1,3 +1,10 @@
 for c in range(0,6):
     print('oi')
 print('Fim')
+for c in range(0,7,2):
+    print(c)
+print('teste')
+
+n = int(input('Digite um número: '))
+for c in range(0, n+1):
+    print(c)
