768b6753d31aa5a28d7ac9bbfc7d6406d9998224
romersonrsantana <romersonsantana@hotmail.com.br>
2024-06-14 23:25:03 -0300

    de passo em passo
    Toda Honra e Toda Glória Ao Deus Vivo, Ao Deus de Israel, Ao Deus de Abraão, Isaac, Jacó e Moisés, e Ao Seu Filho Amado Jesus Cristo. Louvado Seja Nosso Senhor Jesus Cristo. Para Sempre Seja Louvado. Amém.

--- desafios/desafios.exercises/des048.py
+++ desafios/desafios.exercises/des048.py
@@ -6,8 +6,13 @@ color = {'blue':'\033[1;29;44m',
 print('\n',' '*27,'{} Desvendando os multiplos de três {}\n'.format(color['blue'],color['clean']))
 print('{}'.format(color['purple']),'==='*27,'{}'.format(color['clean']),'\n')
 #algoritmos
+s = 0
 for c in range(0,500,3):
+    s = s + 500
     print(c,end=' ')
+    
 print('...')
 print()
 print('{}'.format(color['purple']),'==='*27,'{}'.format(color['clean']),'\n')
+print('A somo dos valores corresponde há {}'.format(s))
+    
