bfd7a42dc77aa8ba827e343109f7082b2c623cd6
romersonrsantana <romersonsantana@hotmail.com.br>
2024-06-16 19:19:02 -0300

    de passo em passo
    Toda Honra e Toda Glória Ao Deus Vivo, Ao Deus de Israel, Ao Deus de Abraão, Isaac e Jacó. E Ao Seu Filho Amado Jesus Cristo. Louvado Seja Nosso Senhor Jesus Cristo. Para Sempre Seja Louvado. Amém.

--- teste/teste005.py
+++ teste/teste005.py
@@ -1,26 +1,55 @@
 import random
 import emoji
-print('\nCorrida Maluca\n')
+from time import sleep
+
+#color palete
+color = {'blue':'\033[1;7;29;44m',
+         'purple':'\033[1;29;45m',
+         'yellow':'\033[1;33m',
+         'green':'\033[1;32m',
+         'gray':'\033[1;7;37m',
+         'clean':'\033[m'}
+#title
+print('\n',' '*18,emoji.emojize("🏁 🏁 🏁 🏁 🏁 🏁"),'{} Corrida Maluca {}'.format(color['blue'],color['clean']),emoji.emojize("🏁 🏁 🏁 🏁 🏁 🏁"), '\n')
 soma = 0
 somcomp = 0
 
-for corrida in range(6):
+for corrida in range(5):
     num = random.randint(1,3)
 
     while True:
-        jogador = int(input('Escolha um número de 1 a 3: ---> '))
+        sleep(1)
+        print(' '*47,emoji.emojize("🔴"))
+        sleep(1)
+        print(' '*47,emoji.emojize("🟠"))
+        sleep(1)
+        print(' '*47,emoji.emojize("🟢"))
+        sleep(1.8)
+        print('\n')
+        print(' '*47,emoji.emojize("🟢"))
+        print(' '*47,emoji.emojize("🟢"))
+        print(' '*47,emoji.emojize("🟢"))
+        sleep(0.9)
+        print()
+        jogador = int(input('{}Escolha um número de 1 a 3: --->{} '.format(color['green'],color['yellow'])))
         if jogador >= 1 and jogador <= 3:
             break
     if jogador == num:
-        print('\nO computador escolheu {} e você escolheu {}\n'.format(num,jogador))
-        print('\nPonto pra você!\n')
-        print(' '*27,emoji.emojize("🚔"))
+        sleep(0.9)
+        print('\n{}O computador escolheu {}\n'.format(color['clean'],num))
+        sleep(1)
+        print('\n',' '*18,emoji.emojize("🚩 🚩 🚩 🚩 🚩 🚩"),'{}  Ponto pra você!  {}'.format(color['purple'], color['clean']),emoji.emojize("🚩 🚩 🚩 🚩 🚩 🚩"),'\n')
+        sleep(0.5)
+        print(' '*47,emoji.emojize("🚔 💨 💨 💨"))
         print()
         soma = soma + 1
     else:
-        print('\nO computador escolheu {} e você escolheu {}\n'.format(num,jogador))
-        print('\nPonto para o computador!!\n')
-        print(' '*27, emoji.emojize("🚘"))
+        sleep(0.9)
+        print('\n{}O computador escolheu {}\n'.format(color['clean'],num))
+        sleep(0.8)
+        print(' '*18,emoji.emojize("🏴‍☠️  🏴‍☠️  🏴‍☠️ "),'{} Ponto para o computador!! {}'.format(color['gray'],color['clean']),emoji.emojize("🏴‍☠️  🏴‍☠️  🏴‍☠️ "),'\n')
+        sleep(0.5)
+        print(' '*47, emoji.emojize("🚘 💨 💨 💨"))
         print()
         somcomp = somcomp + 1
     
@@ -29,8 +58,10 @@ print('Total de acertos jogador {}'.format(soma))
 print('Total de acertos computador {}\n'.format(somcomp))
 
 if soma > somcomp:
-    print('\n Você venceu!! \n')
+    print(' '*27, emoji.emojize("🏆 🏆 🏆"), '{} Você venceu!! {}'.format(color['purple'],color['clean']) , emoji.emojize("🏆 🏆 🏆"), '\n')
+    print('\n',' '*36, emoji.emojize("🏁 🏁 🏁 🚔 🏁 🏁 🏁"), '\n')
 elif soma == somcomp:
     print('\n Empate! \n')
 else:
-    print('\n Vitória do computador! \n')
+    print(' '*27, emoji.emojize("🏆 🏆 🏆"),  '{} Vitória do computador! {}'.format(color['gray'],color['clean']), emoji.emojize("🏆 🏆 🏆"))
+    print('\n',' '*36, emoji.emojize("🏁 🏁 🏁 🚘 🏁 🏁 🏁"), '\n')
