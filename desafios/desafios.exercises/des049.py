a5d44cd51e3f7515d6711e784c03f7073a565a89
romersonrsantana <romersonsantana@hotmail.com.br>
2024-06-17 20:31:49 -0300

    de passo em passo
    Toda Honra e Toda Glória Ao Deus Vivo, Ao Deus de Israel, Ao Deus de Abraão, Isaac, Jacó e Moisés e Ao Seu Filho Amado Jesus Cristo. Louvado Seja Nosso Senhor Jesus Cristo. Para Sempre Seja Louvado. Amém.

--- desafios/desafios.exercises/des049.py
+++ desafios/desafios.exercises/des049.py
@@ -1,5 +1,8 @@
 print('\nTabuada com estrutura de repetição\n')
 
+while True:
+    number = int(input('Insira um número inteiro entre 0 e 10: '))
+    if number >= 0 and number < 11:  
+        break
 for c in range(0,11):
-    soma = soma *  5
-    
+    print('{} x {} = {}'.format(number, c, number*c))
