cb34c7cd8cb26a4ec5d169c62b8a6f819e6059a1
romersonrsantana <romersonsantana@hotmail.com.br>
2024-06-17 22:29:30 -0300

    De passo em passo
    Toda Honra e Toda Glória Ao Deus Vivo, Ao Deus de Israel, Ao Deus de Abraão, Isaac, Jacó e Moisés e Ao Seu Filho Amado Jesus Cristo. Louvado Seja Nosso Senhor Jesus Cristo. Para Sempre Seja Louvado. Amém.

--- desafios/desafios.exercises/des049.py
+++ desafios/desafios.exercises/des049.py
@@ -1,8 +1,19 @@
+import emoji
 print('\nTabuada com estrutura de repetição\n')
 
 while True:
     number = int(input('Insira um número inteiro entre 0 e 10: '))
     if number >= 0 and number < 11:  
         break
-for c in range(0,11):
-    print('{} x {} = {}'.format(number, c, number*c))
+print()
+print(emoji.emojize("🟪🟪🟪🟪🟪🟪🟪🟪🟪🟪🟪🟪🟪🟪🟪🟪🟪🟪🟪🟪🟪🟪"*2))
+print()
+
+for c in range(0, 11):
+    print(' '*27,'{} x {} = {}'.format( number, c, number*c))
+
+print()
+print(emoji.emojize("🟪🟪🟪🟪🟪🟪🟪🟪🟪🟪🟪🟪🟪🟪🟪🟪🟪🟪🟪🟪🟪🟪"*2))
+    
+    
+   
