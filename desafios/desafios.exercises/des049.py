20cefe425f8801625010d849352a09644112995c
romersonrsantana <romersonsantana@hotmail.com.br>
2024-06-18 13:57:25 -0300

    de passo em passo
    Toda Honra e Toda Glória Ao Deus Vivo, Ao Deus de Israel, Ao Deus de Abraão, Isaac, Jacó e Moisés e Ao Seu Filho Amado Jesus Cristo. Louvado Seja Nosso Senhor Jesus Cristo. Para Sempre Seja Louvado. Amém.

--- desafios/desafios.exercises/des049.py
+++ desafios/desafios.exercises/des049.py
@@ -6,14 +6,43 @@ while True:
     if number >= 0 and number < 11:  
         break
 print()
-print(emoji.emojize("🟪🟪🟪🟪🟪🟪🟪🟪🟪🟪🟪🟪🟪🟪🟪🟪🟪🟪🟪🟪🟪🟪"*2))
+while True:
+    print(emoji.emojize("🟦🟦🟦🟦"*9))
+    print()
+    choice = int(input('''Escolha uma opção de tabuada:
+      1 = Tabuada de Subtração
+      2 = Tabuada de Adição
+      3 = Tabuada de Multiplicação
+      4 = Tabuada de Divisão
+        ---> '''))
+    if choice > 0 and choice < 5:
+        print()
+        print(emoji.emojize("🟦🟦🟦🟦"*9))
+        break
+print()
+if choice == 1:
+    print(emoji.emojize("🟧🟧🟧🟧"*9))
+    if number == 0:  
+        for c in range(0, 11):
+            print(' '*27, '{} - {} = {}'.format(c, number, (c - number)))       
+    elif number == 1:
+        for c in range(1, 12):
+            print(' '*27, '{} - {} = {}'.format(c, number, (c - number)))
+    elif number == 2:
+        for c in range(2, 13):
+            print(' '*27,)
+        
+print()
+        print(emoji.emojize("🟧🟧🟧🟧*9"))
+
+print(emoji.emojize("🟪🟪🟪🟪"*9))
 print()
 
 for c in range(0, 11):
-    print(' '*27,'{} x {} = {}'.format( number, c, number*c))
+    print(' '*27,'{} x {} = {}'.format(number, c, number*c))
 
 print()
-print(emoji.emojize("🟪🟪🟪🟪🟪🟪🟪🟪🟪🟪🟪🟪🟪🟪🟪🟪🟪🟪🟪🟪🟪🟪"*2))
+print(emoji.emojize("🟪🟪🟪🟪"*9))
     
     
    
