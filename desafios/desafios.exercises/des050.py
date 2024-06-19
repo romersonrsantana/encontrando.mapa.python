3226bcfeab4e78991f8af05eb2794c553eaffa1d
romersonrsantana <romersonsantana@hotmail.com.br>
2024-06-19 18:27:40 -0300

    De passo em passo
    Toda Honra e Toda Glória Ao Deus Vivo, Ao Deus de Israel, Ao Deus de Abraão, Isaac, Jacó e Moisés e Ao Seu Filho Amado Jesus Cristo. Louvado Seja Nosso Senhor Jesus Cristo. Para Sempre Seja Louvado. Amém.

--- desafios/desafios.exercises/des050.py
+++ desafios/desafios.exercises/des050.py
@@ -3,12 +3,11 @@ s = 0
 print('Digite seis valores e iremos somar apenas os pares:')
 print()
 for c in range(1, 7):
-    choice = int(input('valor {} : --->' .format(c)))
+    choice = int(input('\nvalor {} : ---> ' .format(c)))
     if choice % 2 == 0:
         s = s + choice
-
-print()
-print('{}', end=' '.format(choice))
+if choice % 2 == 0:
+    print(choice)
 print('A soma dos números pares são {}'.format(s))
 print()
 
