d78727e4fe7c2fd44f68fca035f441b302c69fbd
romersonrsantana <romersonsantana@hotmail.com.br>
2024-06-19 21:15:39 -0300

    De passo em passo
    Toda Honra e Toda Glória Ao Deus Vivo. Ao Deus de Israel, Ao Deus de Abraão, Isaac, Jacó e Moisés. Louvado Seja Nosso Senhor Jesus Cristo. Para Sempre Seja Louvado. Amém.

---
+++ aulas/material-complementar/livro002.py
@@ -0,0 +1,9 @@
+print('\n',' '*27,'Chamada de função para saudação\n')
+#Chamada de Função e parâmetro
+def Hello(name):
+    print('\nOlá, ' + name +'!!\n')
+#Dados usuários
+n = input(('Write your name: --> '))
+#Chamada de função com argumento
+Hello('{}'.format(n))
+#Louvado Seja Nosso Senhor Jesus Cristo. Para Sempre Seja Louvado. Amém.
