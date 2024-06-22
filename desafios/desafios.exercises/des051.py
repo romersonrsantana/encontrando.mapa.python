7e6f81ba258a775467ffb471141e39d314e74652
romersonrsantana <romersonsantana@hotmail.com.br>
2024-06-21 21:28:07 -0300

    de passo em passo
    Toda Honra e Toda Glória Ao Deus Vivo, Ao Deus de Israel, Ao Deus de Abraão, Isaac, Jacó e Moisés e Ao Seu Filho Amado Jesus Cristo. Louvado Seja Nosso Senhor Jesus Cristo. Para Sempre Seja Louvado. Amém.

--- desafios/desafios.exercises/des051.py
+++ desafios/desafios.exercises/des051.py
@@ -1,14 +1,20 @@
 from time import sleep
-#Palete 
+#Palete Color
+color = {'blue':'\033[1;29;44m',
+         'purple':'\033[1;29;45m',
+         'green':'\033[1;32m',
+         'yellow':'\033[1;33m',
+         'gray':'\033[1;37m',
+         'clean':'\033[m'}
 
-print('\n',' '*27,'Progressão Aritmética')
+print('\n',' '*27,'{} Progressão Aritmética {}'.format(color['purple'], color['clean']))
 
-first = int(input('\nDigite um número inteiro que será o primeiro termo de uma P.A: ---> '))
+first = int(input('\n{}Digite um número inteiro que será o primeiro termo de uma P.A: ---> {}  '.format(color['gray'],color['blue'])))
 
-reason = int(input('\nInforme um número inteiro que será a razão da P.A ---> '))
-print()
+reason = int(input('\n{}{}Informe um {}número inteiro{} que será a razão da P.A ---> {}  '.format(color['clean'],color['gray'], color['green'], color['gray'], color['blue'])))
+print('{}'.format(color['clean']))
 sleep(0.7)
-print('Calculando os 10 primeiros termos dessa P.A\n')
+print('{}Calculando os {}10 primeiros termos{} dessa P.A{}\n'.format(color['gray'], color['yellow'], color['gray'], color['clean']))
 print()
 sleep(1.6)
 twentieth = first + reason*9
@@ -16,5 +22,4 @@ twentieth = first + reason*9
 for c in range(first, twentieth + 1, reason):
     print(c,'-', end=' ')
 print()
-print('\nProgressão Aritmética concluida com sucesso!! \n')
-
+print('\n',' '*18,'{}  Progressão Aritmética concluida com sucesso!!  2{}\n'.format(color['blue'],color['clean']))
