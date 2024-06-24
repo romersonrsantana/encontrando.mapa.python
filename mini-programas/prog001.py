f453f15dc40b6f963da2e08a00fd02f7691ce846
romersonrsantana <romersonsantana@hotmail.com.br>
2024-06-24 20:33:29 -0300

    De passo em passo
    Toda Honra e Toda Glória Ao Deus Vivo, Ao Deus de Israel, Ao Deus de Abraão, Isaac, Jacó e Moisés e Ao Seu Filho Amado Jesus Cristo. Louvado Seja Nosso Senhor Jesus Cristo. Para Sempre Seja Louvado. Amém.

--- mini-programas/prog001.py
+++ mini-programas/prog001.py
@@ -20,11 +20,21 @@ print('''Para prossegui com o programa, tenham em mãos:
 
 #dados de entrada do usuário:
 amont = None
-installmentValue = None
+installment = None
+cond = None
 
 while True:
-    print('Informe o valor total da compra: ---> ')
-    amont= input()
-    if len(amont) >= 1:
-         break
-
+    amont = float(input('\nInforme o valor total da compra: ---> '))
+    if amont >= 5000 and amont <= 300000:
+        break
+    else:
+        print('\nOs valores deverão estar entre R$ 5.000,00 e R$ 300.000,00')
+while True:
+    installment = float(input('\nInforme a quantidade de parcelas que deseja: ---> '))
+    cond = amont/installment    #parcelas informadas cliente
+    porcentagem = amont*0.02    #2%
+    if porcentagem < cond:
+        break
+    else:
+        print('\nO valor mínimo de cada parcela deverá ser de 2% do valor do veículo.\nOs dados informados correspondem a uma parcela de R$ {}, \nmenos de 2% do valor total de R${}. \nConsidere parcelas acima de R$ {}'.format(cond, amont, amont*0.02))
+print('Fim')
