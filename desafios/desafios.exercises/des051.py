5258b39451ff99b4a8ae45f26f8b8ad64fc8b209
romersonrsantana <romersonsantana@hotmail.com.br>
2024-06-24 21:59:52 -0300

    De passo em passo
    Toda Honra e Toda Glória Ao Deus Vivo, Ao Deus de Israel, Ao Deus de Abraão, Isaac, Jacó e Moisés e Ao Seu Filho Amado Jesus Cristo. Louvado Seja Nosso Senhor Jesus Cristo. Para Sempre Seja Louvado. Amém.

--- desafios/desafios.exercises/des051.py
+++ desafios/desafios.exercises/des051.py
@@ -8,7 +8,7 @@ color = {'blue':'\033[1;29;44m',
          'clean':'\033[m'}
 
 print('\n',' '*27,'{} Progressão Aritmética {}'.format(color['purple'], color['clean']))
-
+022
 first = int(input('\n{}Digite um número inteiro que será o primeiro termo de uma P.A: ---> {}  '.format(color['gray'],color['blue'])))
 
 reason = int(input('\n{}{}Informe um {}número inteiro{} que será a razão da P.A ---> {}  '.format(color['clean'],color['gray'], color['green'], color['gray'], color['blue'])))
@@ -17,9 +17,9 @@ sleep(0.7)
 print('{}Calculando os {}10 primeiros termos{} dessa P.A{}\n'.format(color['gray'], color['yellow'], color['gray'], color['clean']))
 print()
 sleep(1.6)
-twentieth = first + reason*9
+t = first + reason*9   #formula para encontrar os dez primeiros da P.A
 
-for c in range(first, twentieth + 1, reason):
+for c in range(first, t + 1, reason): #Algoritmo
     print(c,'-', end=' ')
-print()
+print('Acabou!!')
 print('\n',' '*18,'{}  Progressão Aritmética concluida com sucesso!!  {}\n'.format(color['blue'],color['clean']))
