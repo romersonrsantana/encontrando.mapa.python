01ad0eda8b893830cf5c077566fdfcb4d92767a2
romersonrsantana <romersonsantana@hotmail.com.br>
2024-06-26 20:39:21 -0300

    de passo em passo
    Louvado Seja Nosso Senhor Jesus Cristo. Para Sempre Seja Louvado. Amém.

--- desafios/desafios.exercises/des052.py
+++ desafios/desafios.exercises/des052.py
@@ -1,5 +1,16 @@
-from datetime import date
-
-ano = date.today().year
-
-print(ano)
+print('\n',' '*27,'Detectando números primos\n')
+#dados usuário
+number = int(input('\nInforme um número inteiro: --> '))
+#condição
+if number % 2 == 0:
+    print('\nO número informado é PAR e COMPOSTO!')
+elif number % 3 == 0:
+    print('\nO número é IMPAR e COMPOSTO!')
+elif number == 2:
+    print('\nO número 2 é PAR e ao mesmo tempo PRIMO!\n')
+elif number == 1:
+    print('\nNão se classifica como número PRIMO.\n')
+elif number == 0:
+    print('\nNão é um número composto porque não atende a caracteristica de ser escrito como um produto dos números primos\n')
+else:
+    print('\nO número informado é PRIMO!')
