fdb120d80ef1ea1df805ac3c7cdb8ac63ba45fd2
romersonrsantana <romersonsantana@hotmail.com.br>
2024-06-27 21:33:53 -0300

    de passo em passo
    Louvado Seja Nosso Senhor Jesus Cristo. Para Sempre Seja Louvado. Amém.

--- desafios/desafios.exercises/des052.py
+++ desafios/desafios.exercises/des052.py
@@ -4,6 +4,8 @@ number = int(input('\nInforme um número inteiro: --> '))
 #condição
 if number % 2 == 0:
     print('\nO número informado é PAR e COMPOSTO!')
+elif number == 3:
+    print(' \nO número informado é IMPAR e PRIMO.\n')
 elif number % 3 == 0:
     print('\nO número é IMPAR e COMPOSTO!')
 elif number == 2:
@@ -13,4 +15,4 @@ elif number == 1:
 elif number == 0:
     print('\nNão é um número composto porque não atende a caracteristica de ser escrito como um produto dos números primos\n')
 else:
-    print('\nO número informado é PRIMO!')
+    print('\nO número informado é PRIMO!\n')
