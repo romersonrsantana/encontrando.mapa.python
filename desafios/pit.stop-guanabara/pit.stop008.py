9ab77327168259c88345ec6d0f26019b2664f97c
romersonrsantana <romersonsantana@hotmail.com.br>
2024-07-05 13:51:04 -0300

    de passo em passo
    Louvado Seja Nosso Senhor Jesus Cristo. Para Sempre Seja Louvado. Amém.

--- desafios/pit.stop-guanabara/pit.stop008.py
+++ desafios/pit.stop-guanabara/pit.stop008.py
@@ -1,6 +1,20 @@
-print('Detectando números primos')
+print('\n',' '*27,'Detectando números primos\n')
 
 number = int(input('Informe um número: '))
-
-for c in range(number, 0, -1):
+print()
+total = 0
+for c in range(1, number + 1):
     if number % c == 0:
+        print('\033[1;34m', end='')
+        total += 1
+    else:
+        print('\033[m', end='')
+    print('{} '.format(c), end='')
+
+print('\n\n\033[mO número {} foi dividido {} vezes'.format(number, total))
+
+if total == 2:
+    print('\nE por isso ele é PRIMO!\n')
+else:
+    print('\nE por isso ele NÃO É PRIMO!\n')
+    
