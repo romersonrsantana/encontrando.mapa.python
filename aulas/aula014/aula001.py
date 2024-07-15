6198f806ce9926f5799058036e935a024c30f545
romersonrsantana <romersonsantana@hotmail.com.br>
2024-07-15 11:49:25 -0300

    de passo em passo
    Senhor Jesus eu confio e espero em Ti. Amém.

---
+++ aulas/aula014/aula001.py
@@ -0,0 +1,8 @@
+r = 'S'
+
+while r == 'S':
+    number  = int(input('\nDigite um valor: --> '))
+
+    r = str(input('Quer continuar? [S/N]')).upper()
+
+#Louvado Seja Nosso Senhor Jesus Cristo. Para Sempre Seja Louvado. Amém.
