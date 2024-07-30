926c42d2fab76a0d0c95d3f2b817658465afb427
romersonrsantana <romersonsantana@hotmail.com.br>
2024-07-30 20:53:54 -0300

    de passo em passo
    Toda Honra e Toda Glória Ao Deus de Abraão, Isaac, Jacó, Israel e Moises e Ao Espirito Santo e a Jesus Cristo. Louvado Seja Nosso Senhor Jesus Cristo. Para Sempre Seja Louvado. Amém.

---
+++ desafios/desafios.exercises/des057.py
@@ -0,0 +1,9 @@
+print('\n',27*' ','Coletando informações\n')
+sexo = '0'
+while sexo != 'M' and sexo != 'F':  
+    sexo = str(input('\nInforme seu sexo, M para MASCULINO ou F para FEMININO --> ')).upper()
+    if sexo == 'M' or sexo == 'F':
+        print('\nValor armazenado com sucesso!\n')
+    else:
+        print('\nOs dados informados não correspondem a solicitação de M ou F para o sexo correspondente!\n')
+print('\nFim\n')
