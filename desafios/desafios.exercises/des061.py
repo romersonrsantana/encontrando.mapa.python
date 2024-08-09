c778548d7861b562a3f19e855bee89e2b0d91740
romersonrsantana <romersonsantana@hotmail.com.br>
2024-08-09 20:57:25 -0300

    de passo em passo
    Senhor Jesus Cristo eu confio em ti, Senhor Jesus Cristo eu espero em Ti. Amém. Louvado Seja Nosso Senhor Jesus Cristo. Para Sempre Seja Louvado. Amém.

--- desafios/desafios.exercises/des061.py
+++ desafios/desafios.exercises/des061.py
@@ -1,16 +1,34 @@
+colors = {'blue':'\033[1;34m',
+          'purple':'\033[1;35m',
+          'alert':'\033[1;31m',
+          'gray':'\033[1;37m',
+          'clean':'\033[m'}
+
+print('\n',' '*27,'{}Arithmetic progression with ten terms{}\n'.format(colors['purple'], colors['clean']))
+
+numero_inicial = int(input('\n{}Enter a number to start the arithmetic progression-->{} '.format(colors['gray'], colors['blue'])))
+
+razao_pa = int(input('\n{}Enter a number that will be the reason for arithmetic programming-->{} '.format(colors['gray'], colors['blue'])))
 
 controle = 0
 
-n = int(input('--> '))
-pa = int(input('--> '))
+print()
 
 while controle < 10:
     controle += 1
 
     if controle == 1:
-        total = n
-        print(total)
+        total = numero_inicial
+        print(f'{colors["purple"]}',total , ' - ' ,end='')
+    
+    elif controle == 10:
+        total += razao_pa
+        (print(total, '.' , end=''))
 
     else:
-        total += pa
-        print(total)
+        total += razao_pa
+        print(total ,' - ',end='')
+
+print(f'{colors["clean"]}\n')
+
+#Louvado Seja Nosso Senhor Jesus Cristo. Para Sempre Seja Louvado. Amém.
