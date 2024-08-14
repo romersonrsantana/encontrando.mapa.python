43c35e01ce548f283d0d7fdec01ecb82ab487976
romersonrsantana <romersonsantana@hotmail.com.br>
2024-08-13 22:14:48 -0300

    de passo em passo
    Sagrado Coração de Jesus em Confio em Ti. Sagrado Coração de Jesus eu Espero em Ti. Amém.

--- desafios/desafios.exercises/des062.py
+++ desafios/desafios.exercises/des062.py
@@ -3,12 +3,42 @@ numero_termos = 11
 total = 0
 
 print()
-n = int(input('--> '))
-r = int(input('--> '))
+
+# ------------ Informações iniciais ---------------
+
+numero_inicial = int(input('\nInforme um número para iniciar --> '))
+
+razao_pa = int(input('Informe a razão da P.A --> '))
+
+# ------------- Estrutura de decisão --------------
+
+# --> O laço vai continuar até que o controlador e o número de termos sejam iguais.
+
+while controlador < numero_termos:
+
+    controlador += 1
+
+    if controlador == 1:
+        total = numero_inicial
+        print('\nTermo', f'{controlador} -->',total)
+
+    elif controlador == numero_termos:
+        controlador = 0
+        continuacao_termos = int(input('\n\nDeseja saber mais termos? \n(informe mais quantos termos deseja saber) \n\n---> '))
+
+        if continuacao_termos == 0:
+            numero_termos = 0
+        else:
+            numero_termos += continuacao_termos
+    
+    else:
+        total += razao_pa
+        print('Termo' , f'{controlador} -->',total)
+
+print('\n\nFim!\n\n')
 
 
-
-while controlador < numero_termos and numero_termos != 0:
+'''while controlador < numero_termos and numero_termos != 0:
 
     controlador += 1
 
@@ -25,6 +55,8 @@ while controlador < numero_termos and numero_termos != 0:
         while controlador < continuacao_termos:
             
             controlador += 1
+            
+           
 
             if continuacao_termos != 0:
                 total += r
@@ -33,14 +65,13 @@ while controlador < numero_termos and numero_termos != 0:
             elif continuacao_termos == controlador:  
                 continuacao_termos = int(input('Deseja saber mais termos --> '))
             
-            else:
-                print('Fim!!')
-
-            
+            #else:
+                #print('\n\nFim!!\n\n') 
+                continuacao_termos = int(input('Deseja saber mais termos --> '))
 
     else:
         total += r
-        print(total)
+        print(total)'''
     
     
 
