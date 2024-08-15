0b03ea32c4eb1c936632724ab4a4f577d11e6fd0
romersonrsantana <romersonsantana@hotmail.com.br>
2024-08-15 18:47:17 -0300

    de passo em passo
    Toda Honra e Toda Glória Ao Deus Vivo, Ao Deus de Abraão, Isaac, Jacó, Israel e Moisés. Toda Honra e Toda Glória Ao Espirito Santo. Toda Honra e Toda Glória A Jesus Cristo. Louvado Seja Nosso Senhor Jesus Cristo. Para Sempre Seja Louvado. Amém.

--- desafios/desafios.exercises/des062.py
+++ desafios/desafios.exercises/des062.py
@@ -1,22 +1,23 @@
-colors = {'Purple':'\033[m',
-          'Blue':'\033[m',
-          'Yellow':'\033[m',
-          'Green':'\033[m',
-          'Alert':'\033[m',
-          'Gray':'\033[m',
+colors = {'Purple':'\033[1;35m',
+          'Blue':'\033[1;34m',
+          'Yellow':'\033[1;33m',
+          'Green':'\033[1;32m',
+          'Alert':'\033[1;31m',
+          'Gray':'\033[1;37m',
           'Clean':'\033[m'}
 
 controlador = 0
 numero_termos = 11
 total = 0
 
-print('\n',' '*27,'Desenvolvendo uma Progressão Aritmética\n')
+print('\n',' '*27,f'{colors["Purple"]}Desenvolvendo uma Progressão Aritmética{colors["Clean"]}\n')
 
 # ------------ Informações iniciais ---------------
 
-numero_inicial = int(input('\nInforme um número para iniciar --> '))
+numero_inicial = int(input('\n{}Informe um número para iniciar -->{} '.format(colors['Gray'],colors['Blue'])))
 
-razao_pa = int(input('Informe a razão da P.A --> '))
+razao_pa = int(input('{}Informe a razão da P.A -->{} '.format(colors['Gray'],colors['Blue'])))
+print(f'{colors["Clean"]}')
 
 # ------------- Estrutura de decisão --------------
 
@@ -28,22 +29,24 @@ while controlador < numero_termos:
 
     if controlador == 1:
         total = numero_inicial
-        print('\nTermo', f'{controlador} -->',total)
+        print('\n{}Termo'.format(colors['Green']), f'{controlador} -->',f'{colors["Yellow"]}',total)
 
     elif controlador == numero_termos:
         
-        continuacao_termos = int(input('\n\nDeseja saber mais termos? \n(informe mais quantos termos deseja saber ou informe zero para encerrar) \n\n---> '))
+        continuacao_termos = int(input('\n\n{}Deseja saber mais termos? \n(informe mais quantos termos deseja saber ou informe zero para encerrar) \n\n--->{} '.format(colors['Gray'],colors['Blue'])))
 
         if continuacao_termos == 0: #condição que permite sair do laço caso o usuário digite 0.
             numero_termos = 0
 
         else: #condição que aumenta o número de termos e ele deixa de ser igual ao controlador.
-            numero_termos += continuacao_termos + 1      
+            numero_termos += continuacao_termos + 1     
     
     else:
         total += razao_pa
-        print('Termo' , f'{controlador} -->',total)
+        print(f'{colors["Green"]}Termo' , f'{controlador} -->{colors["Yellow"]}',total)
 
-print('\n\nFim!\n\n')
+print('\n\n',' '*27,f'{colors["Purple"]}Fim!{colors["Clean"]}\n\n')
+
+#Na segunda parte que é a continuação dos termos o contador dos termos apresenta a continuação pra mais, pois considera a pergunta.
 
 #Louvado Seja Nosso Senhor Jesus Cristo. Para Sempre Seja Louvado. Amém.
