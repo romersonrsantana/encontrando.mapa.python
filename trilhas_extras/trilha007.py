3e5c0e54cec2feb2f8827b0c68ed440b328f92c5
romersonrsantana <romersonsantana@hotmail.com.br>
2024-08-19 09:06:56 -0300

    de passo em passo
    Louvado Seja Nosso Senhor Jesus Cristo. Para Sempre Seja Louvado. Amém.

---
+++ trilhas_extras/trilha007.py
@@ -0,0 +1,11 @@
+print('\nTeste de funções')
+
+def hello():
+    print('Howdy!')
+    print('Howdy!!')
+    print('Hello there')
+
+hello()
+print('\ncalling the function!\n')
+hello()
+print('\nThe end!\n')
---
+++ trilhas_extras/trilha008.py
@@ -0,0 +1,13 @@
+print('\n',' '*27,'Test calling function\n')
+
+def hello(name):
+    print('Hello, ' + name + '!!\n')
+
+hello(' Tereza')
+hello(' João')
+
+name = str(input('\nWhat is your name? \n\n--> '))
+print()
+hello(name)
+print()
+#Louvado Seja Nosso Senhor Jesus Cristo. Para Sempre Seja Louvado. Amém.
