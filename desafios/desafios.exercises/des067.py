039705557007439843b736d781be6eb78e856e29
romersonrsantana <romersonsantana@hotmail.com.br>
2024-08-27 20:55:49 -0300

    de passo em passo
    Sagrado Coração de Jesus, confio e espero em Vós. Amém.

--- desafios/desafios.exercises/des067.py
+++ desafios/desafios.exercises/des067.py
@@ -1,29 +1,19 @@
-colors = {'blue':'\033[1;34m',
-                'purple':'\033[1;35m',
-                'alert':'\033[1;31m',
-                'gray':'\033[1;37m',
-                'clean':'\033[m'}
-
-
-
-print('\n',' '*27,f'{colors["purple"]}Multiplication table!{colors["clean"]}\n')
-
-total = counter = 0
+print('\n',' '*27,'Multiplication table\n')
 
 while True:
     try:
-        number = int(input(f'\n{colors["gray"]}Enter an interger:\n\n-->{colors["blue"]} '))
-
-        if number == 999:
+        number = int(input('\nEnter a number (or enter a negative number to end.):\n\n--> '))
+        if number < 0:
             break
-        
-        counter += 1
-        total += number
-
     except:
-        print('\n{}[Alert]{} Enter a valid number!{}\n'.format(colors['alert'], colors['gray'], colors['clean']))
-    continue
+        print("\n[Alert!] You didn't enter a whole number!\n")
+        continue
 
-print(f'\n{colors["blue"]}-->{colors["purple"]} {counter} {colors["blue"]}numbers were added,\n\n--> Total sum {colors["purple"]} {total} {colors["blue"]}.{colors["clean"]}\n\n')
+    print('~'*18)
 
-#Toda Honra e Toda Glória Ao Deus de Abraão, Isaac, Jacó, Israel e Moisés. Toda Honra e Toda Glória Ao Espiríto Santo. Toda Honra e Toda Glória A Jesus Cristo. Louvado Seja Nosso Senhor Jesus Cristo. Para Sempre Seja Louvado. Amém.
+    for c in range(0, 11):
+        print(' '*3,f'{c:^3} x {number} = {c*number}')
+    
+    print('~'*18)
+
+print('\nMultiplication table completed successfully.\n')
---
+++ desafios/desafios.exercises/des068.py
@@ -0,0 +1,11 @@
+from time import sleep
+
+print(f"{' '*27}\nLet's Go to play?")
+sleep(2.1)
+print(f"\n{' '*27}--> Pair", end='')
+sleep(2.1)
+print(f"{' '*27}--> Odd\n")
+
+while True:
+
+#Toda Honra e Toda Glória Ao Deus de Abraão, Isaac, Jacó, Israel e Moisés. Toda Honra e Toda Glória Ao Espiríto Santo. Toda Honra e Toda Glória A Jesus Cristo. Louvado Seja Nosso Senhor Jesus Cristo. Para Sempre Seja Louvado. Amém.
