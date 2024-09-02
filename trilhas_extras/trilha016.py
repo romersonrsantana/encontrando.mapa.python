957177cd763d43869fe9ac0bacc9fa7d525ef639
romersonrsantana <romersonsantana@hotmail.com.br>
2024-09-02 17:16:50 -0300

    de passo em passo
    Sagrado Coração de Jesus, confio e espero em Vós. Amém.

---
+++ trilhas_extras/trilha016.py
@@ -0,0 +1,16 @@
+
+
+def spam(divide_by):
+
+    try:
+        return 42/divide_by
+    except ZeroDivisionError:
+        print('\n[ERROR: Invalid argument.]\n')
+
+print('\n',spam(2))
+print('\n',spam(12))
+print('\n',spam(0))
+print('\n',spam(1))
+print()
+
+#Toda Honra e Toda Glória Ao Deus de Abraão, Isaac, Jacó, Israel e Moisés. Toda Honra e Toda Glória Ao Espiríto Santo. Toda Honra e Toda Glória a Jesus Cristo. Louvado Seja Nosso Senhor Jesus Cristo. Para Sempre Seja Louvado. Amém.
