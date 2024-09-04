ba66a471e5b08320893f88d5040d3cc0f7458a8a
romersonrsantana <romersonsantana@hotmail.com.br>
2024-09-04 18:09:01 -0300

    de passo em passo
    Sagrado Coração de Jesus confio e espero em vós. Amém.

--- desafios/desafios.exercises/des070.py
+++ desafios/desafios.exercises/des070.py
@@ -1,4 +1,7 @@
-print('\nPurchasing control\n') #controle de compras
+print()
+print('==='*27)
+print('\n',' '*27,'Purchasing control\n') #controle de compras
+print('==='*27)
 
 control = total = sum_of_products = 0
 
@@ -12,26 +15,34 @@ while boolean != True:
 
     while True:
 
-        product = str(input(f'\nEnter the name of the {control}st product: ')).strip()
+        product = str(input(f'\n--> Enter the name of the {control}st product: ')).strip()
 
         if product[:3].isalpha() == True:
             break
         else:
-            print('\nThe product name cannot start with numbers.\n') #Ñ é permitido o nome do produto começar com num.
+            print('~~~'*27)
+            print('\n[Alert]The product name cannot start with numbers.\n') #Ñ é permitido o nome do produto começar com num.
+            print('~~~'*27)
     
     while True:
 
         try:
-            price = float(input(f'\nInform the price of the {control}st product: u$ '))
+            print('==='*27)
+            price = float(input(f'\n--> Inform the price of the {control}st product: u$ '))
+            print('==='*27,'\n')
 
             if price <= 1 or price > 71000:
+                print('~~~'*27)
                 print("\nThis value isn't accepted.\n\nAllowed values between U$ 1 and U$ 71000")
+                print('~~~'*27,'\n')
             
             else:
                 break
 
         except:
+            print('~~~'*27)
             print('\n[Alert] Enter a numerical value.\n')
+            print('~~~'*27)
             continue
     
     if price > 1000:
@@ -48,7 +59,8 @@ while boolean != True:
 
     while True:
 
-        to_be = str(input('\nWant to continue? [Y/N]')).strip().lower()[0]
+        to_be = str(input('\n--> Want to continue? [Y/N]')).strip().lower()[0]
+        print('\n','==='*27)
 
         if to_be == 'n':
             boolean = True
@@ -56,10 +68,13 @@ while boolean != True:
         elif to_be == 'y':
             break
         else:
+            print('==='*27)
             print("\n[Alert] Unidentified answers ('y' to yes or 'n' to not)\n")
-
+            print('==='*27)
+print('---'*27)
 print(f'\n>>> Total expenses US {total}.\n')
 print(f'\n>>> {sum_of_products} products above US 1.000 were identified.')
 print(f'\n>>> The cheapest product was: --> {lowest_priced_product}\n')
+print('---'*27)
             
 #Toda Honra e Toda Glória Ao Deus de Abraão, Isaac, Jacó, Israel e Moisés. Toda Honra e Toda Glória Ao Espiríto Santo. Toda Honra e Toda Glória A Jesus Cristo. Louvado Seja Nosso Senhor Jesus Cristo. Para Sempre Seja Louvado. Amém.
