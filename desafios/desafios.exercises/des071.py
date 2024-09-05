63c5a663f1ffdbeafc445e22b8823b988e60dde4
romersonrsantana <romersonsantana@hotmail.com.br>
2024-09-04 21:35:37 -0300

    de passo em passo
    Sagrado Coração de Jesus confio e espero em Vós. Amém.

--- desafios/desafios.exercises/des071.py
+++ desafios/desafios.exercises/des071.py
@@ -1,37 +1,58 @@
-print('teste')
-n = int(input('Number --> '))
+print('==='*18)
+print(' '*27,'Atm')
+print('==='*18)
+print('\n\n','---'*7 + ' Sake ' + '---'*7)
 
-int = resto = resto2 = troco = resto3 = troco2 = resto4 = 0
+inteiro = resto = resto2 = troco = resto3 = troco2 = resto4 = 0
+boolean = False
 
-while True:
-    if n > 50:
-        int = n//50
-        resto = n % 50
+while boolean != True:
+    sake = int(input('\nWhat amount do you want to withdraw?\n\n--> '))
 
-        if n >= 100:
-            print('O máximo de notas diponiveis de 50 é 100.\n\nPor favor refaça a operação!!\n')
-            exit()
+    while True:
+        if sake > 50:
+            inteiro = sake//50
+            resto = sake % 50
 
-        elif resto == 0:
-            break
+            if inteiro >= 100:
+                print('O máximo de notas diponiveis de 50 é 100.\n\nPor favor refaça a operação!!\n')
+                exit()
 
-        else:
-            resto2 = resto//20
-            troco = resto % 20
-            if troco == 0:
+            elif resto == 0:
                 break
+
             else:
-                resto3 = troco//10
-                troco2 = troco % 10
-                if troco2 == 0:
+                resto2 = resto//20
+                troco = resto % 20
+                if troco == 0:
                     break
                 else:
-                    resto4 = troco2//1
-                    break
-    
-print(f'Nota de R$ 50. --> {int}')
-print(f'Nota de R$ 20. --> {resto2}')
-print(f'Nota de R$ 10. --> {resto3}')
-print(f'Nota de R$ 1. --> {resto4}')
+                    resto3 = troco//10
+                    troco2 = troco % 10
+                    if troco2 == 0:
+                        break
+                    else:
+                        resto4 = troco2//1
+                        break
+    print('---'*7 + ' Quantity of banknotes for whithdrawal ' + '---'*7)
+    print(f'\nNota de R$ 50,00 --> {inteiro}.')
+    print(f'\nNota de R$ 20,00 --> {resto2}.')
+    print(f'\nNota de R$ 10,00 --> {resto3}.')
+    print(f'\nNota de R$ 1,00 --> {resto4}.\n')
+    print('---'*27)
+
+    while True:
+        choice = str(input('\nDo you want to make any more withdrawals?[y] yes or [N] not \n\n--> ')).strip().lower()
+
+        if choice == 'y':
+            break
+        elif choice == 'n':
+            boolean = True
+            break
+        else:
+            print("\n[Alert!] Choose [Y] for 'yes' or [N] to 'not'.\n")
+print('\n'+'==='*27)
+print(' '*27,'Program ended successfully')
+print('==='*27+'\n')
 
 #Toda Honra e Toda Glória Ao Deus de Abraão, Isaac, Jacó, Israel e Moisés. Toda Honra e Toda Glória Ao Espiríto Santo. Toda Honra e Toda Glória A Jesus Cristo. Louvado Seja Nosso Senhor Jesus Cristo. Para Sempre Seja Louvado. Amém.
