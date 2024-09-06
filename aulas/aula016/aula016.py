40257b160e86385ee2df56d75fd02f68892b3fff
romersonrsantana <romersonsantana@hotmail.com.br>
2024-09-06 15:01:14 -0300

    de passo em passo
    Sagrado Coração de Jesus, confio e espero em Vós. Amém.

---
+++ aulas/aula016/aula016.py
@@ -0,0 +1,17 @@
+lanche = ('hamburger','suco','pizza','Pudim')
+print()
+print(lanche)
+print()
+print(lanche[1],'\n')
+print(lanche[:3],'\n')
+print(lanche[:4],'\n')
+print(lanche[3],'\n')
+
+for c in lanche: 
+    print(c)
+
+    if c == 'Pudim':
+
+        print('\nO Pudim de Mamãe era o melhor do mundo!\nTe amo Mamãe Tereza S2!\n')
+    
+#Toda Honra e Toda Glória Ao Deus de Abraão, Isaac, Jacó, Israel e Moisés e Ao Seu Espiríto Santo e Ao Seu Filho Amado Jesus Cristo. Louvado Seja Nosso Senhor Jesus Cristo, Para Sempre Seja Louvado. Amém.
---
+++ aulas/aula016/aula016pj.py
@@ -0,0 +1,16 @@
+lanche = ('hamburger','suco','pizza','Pudim')
+
+for c in range(0, len(lanche)):
+
+    print(f'\nEu vou beber {lanche[c]}.\n') if c == 1 else print(f'\nEu vou comer {lanche[c]}.\n')
+
+    if c == 3:
+        print(f'\nO {lanche[c]} de Mamãe encantava muita gente com o seu delicioso sabor!\nTe amo Mamãe Tereza S2!\n')
+    elif c == 1:
+        print(f'\nJa bebi {c}...\n')
+
+        print(f'\nJá bebi do suco que Mamãe preparava com tanto carinho!\nMeu Amor por Você Mamãe será de janeiro a janeiro! Até depois de o mundo acabar!\n')
+    else:
+        print(f'\nJá comi {c+1}...\n')
+
+#Toda Honra e Toda Glória Ao Deus de Abraão, Isaac, Jacó, Israel e Moisés E Ao Seu Espiríto Santo E Ao Seu Filho Amado Jesus Cristo. Louvado Seja Nosso Senhor Jesus Cristo. Para Sempre Seja Louvado. Amém.
