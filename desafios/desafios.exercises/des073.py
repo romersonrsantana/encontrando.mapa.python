455528eef5d949511df798af70f090e574cb1009
romersonrsantana <romersonsantana@hotmail.com.br>
2024-09-10 18:41:09 -0300

    de passo em passo
    Sagrado Coração de Jesus confio e espero em Vós. Amém.

--- desafios/desafios.exercises/des073.py
+++ desafios/desafios.exercises/des073.py
@@ -47,14 +47,33 @@ while True:
         print(sorted(position))
     
     elif choice == 4:
-        print()
-        your_team = str(input('write your team: --> ')).strip().title()
+         
+            for c in range(1, 4):
+                print()
+                your_team = str(input('write your team: --> ')).strip().title()
+                print('---'*27)
+
+                try:
+                    print(f'Your team {your_team} is in ',position.index(your_team) + 1,'° position.')
+                    #print(c) --> Quando break o contador é reiniciado.
+                    break
+                
+                except:
+                    print('[Alert] Your team has not been identified, make sure the name has been written correctly and that it is part of the series A table.')
+
+                    if c == 3:
+                        print('---'*27)
+                        print('[Program closed]\n\nYou entered the name of the wrong club three times, make sure the name was spelled correctly (with accents), that the team belongs to Serie A.')
+                        print('---'*27)
+                        exit()
+
+    elif choice == 5:
+        print('\nProgram ended sucessfully.\n')
+        print('---'*27)
+        exit()
+
+    else:
+        print('[Alert] Enter values between one and five!')
         print('---'*27)
 
-        try:
-            print(f'Your team {your_team} is in ',position.index(your_team) + 1,'° position.')
-        except:
-            print('[Alert] Your team has not been identified, make sure the name has been written correctly and that it is part of the series A table.')
-            continue
-        
 #Toda Honra e Toda Glória Ao Deus de Abraão, Isaac, Jacó, Israel e Moisés Ao Seu Espiríto Santo E Ao Seu Filho Amado Jesus Cristo. Louvado Seja Nosso Senhor Jesus Cristo, Para Sempre Seja Louvado. Amém. 
