fe484b1a890772a0de972f1179023d84260a1e53
romersonrsantana <romersonsantana@hotmail.com.br>
2024-09-10 14:42:03 -0300

    de passo em passo
    Louvado Seja Nosso Senhor Jesus Cristo, Para Sempre Seja Louvado. Amém.

---
+++ trilhas_extras/trilha019.py
@@ -0,0 +1,9 @@
+print()
+print('º¨¨¨º¨'*9)
+
+spam = [['cat', 'bat'], [10, 20, 30, 40, 50, [16, 28]]]
+
+print(spam[1][5][0], '\n')
+print(spam[-1][-1][-2], '\n')
+
+#Toda Honra e Toda Glória Ao Deus de Abraão, Isaac, Jacó, Israel e Moisés, Ao Seu Espiríto Santo E Ao Seu Filho Amado Jesus Cristo. Louvado Seja Nosso Senhor Jesus Cristo, Para Sempre Seja Louvado. Amém.
---
+++ trilhas_extras/trilha020.py
@@ -0,0 +1,9 @@
+spam = [1, 2, 3, 4, 5, [10, 20, 30, [16, 28]]]
+print(len(spam))
+
+print(spam[5][3])
+spam[5] = spam[5][3]
+
+print(spam)
+
+#Toda Honra e Toda Glória Ao Deus de Abraão, Isaac, Jacó, Israel e Moisés, Ao Seu Espiríto Santo E Ao Seu Filho Amado Jesus Cristo. Louvado Seja Nosso Senhor Jesus Cristo. Para Sempre Seja Louvado. Amém.
