6ebb63cc9be9b9a06c6ac11e8cbe0735bf4700dd
romersonrsantana <romersonsantana@hotmail.com.br>
2024-09-11 19:50:07 -0300

    de passo em passo
    Sagrado Coração de Jesus, confio e espero em Vós. Amém.

--- desafios/desafios.exercises/des074.py
+++ desafios/desafios.exercises/des074.py
@@ -1,7 +1,25 @@
+import random
 print()
 print('==='*27)
 print(' '*27,'Generating random numbers in tuples')
 
+choise = (random.randint(1, 16), random.randint(1, 28), random.randint(1, 34), random.randint(1, 37), random.randint(1, 111))
 
+for c in range(len(choise)):
+    if c == 0:
+        smaller = choise[c]
+        biggest = choise[c]
+    else:
+        if choise[c] < smaller:
+            smaller = choise[c]
+
+        elif biggest < choise[c]:
+            biggest = choise[c]
+
+print(choise)
+
+print(f'\nThe smaller number is in position {choise.index(smaller)}, with the value of {smaller:-^18}.')
+
+print(f'\nThe biggest number is in position {choise.index(biggest)}, with the value of {biggest:-^18}.\n')
 
 #Toda Honra e Toda Glória Ao Deus de Abraão, Isaac, Jacó, Israel e Moisés, Ao Seu Espiríto Santo E Ao Seu Filho Amado Jesus Cristo. Louvado Seja Nosso Senhor Jesus Cristo, Para Sempre Seja Louvado. Amém.
---
+++ desafios/desafios.exercises/des075.py
@@ -0,0 +1,40 @@
+print()
+print('¨¨¨¨==='*9)
+print(' '*27,'Reading values')
+print('======='*9)
+
+test1 = int(input('\nEnter the first number --> '))
+test2 = int(input('\nEnter the second number --> '))
+test3 = int(input('\nEnter the third number --> '))
+test4 = int(input('\nEnter the fourth number --> '))
+
+total = ''
+
+test = (test1, test2, test3, test4)
+
+for c in range(len(test)):
+    if test[c] == 3:
+        repeat = test.index(3)
+        break
+    else:
+        repeat = " Didn't appear once "
+
+print()
+print('¨¨¨'*27)
+print('The numbers reported are ' + str(test) + '.')
+
+print(f'\n>>> The number (9) nine appeared {test.count(9)} times.\n')
+
+print('\n>>> The first time the number (3) three appeared, was in position ' + str(repeat) + '.\n')
+
+print('==='*27)
+print(' '*27,'Numbers pairs')
+print('==='*27)
+
+for c in range(len(test)):
+    if test[c] % 2 == 0:
+        print(f'{test[c]} ', end='')
+
+print(f'\n',' '*27,'Program completed successfully!\n')
+
+#Toda Honra e Toda Glória Ao Deus de Abraão, Isaac, Jacó, Israel e Moisés, Ao Seu Espírito Santo e Ao Seu Filho Amado Jesus Cristo. Louvado Seja Nosso Senhor Jesus Cristo, Para Sempre Seja Louvado. Amém.
