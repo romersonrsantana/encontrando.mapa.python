38d77e75e1f40957b0e482691f9a030a30a461b2
romersonrsantana <romersonsantana@hotmail.com.br>
2024-09-12 14:36:19 -0300

    de passo em passo
    Sagrado Coração de Jesus confio e espero em Vós. Amém.

---
+++ trilhas_extras/trilha023.py
@@ -0,0 +1,15 @@
+print('==='*27)
+print(' '*27, 'My Pets')
+print('==='*27)
+
+my_pets = ['zophie', 'pooka', 'fat-tail']
+
+print('\nEnter a pet name:\n')
+name = str(input('--> ')).lower()
+
+if name not in my_pets:
+    print('\nI do not have a pet named ' + name + '.\n')
+else:
+    print('\n' + name + ' is my pet.\n')
+
+#Toda Honra e Toda Glória Ao Deus de Abraão, Isaac, Jacó, Israel e Moisés Ao Seu Espírito Santo E Ao Seu Filho Amado Jesus Cristo. Louvado Seja Nosso Senhor Jesus Cristo, Para Sempre Seja Louvado. Amém.
