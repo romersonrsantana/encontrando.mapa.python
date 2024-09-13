7c702c23aa4d7b9198357ecce53764ccf5a4b1b8
romersonrsantana <romersonsantana@hotmail.com.br>
2024-09-13 15:02:18 -0300

    de passo em passo
    Sagrado Coração de Jesus confio e espero em Vós. Amém.

---
+++ trilhas_extras/trilha024.py
@@ -0,0 +1,18 @@
+import random
+print('==='*27)
+print(' '*27, 'Magic e Ball')
+print('==='*27)
+
+messagens = ["It's certain",
+             "It's decidedly so",
+             "Yes definitely",
+             "Reply Hazy try again",
+             'Ask again later',
+             'Concentrate and ask again',
+             'My reply is no',
+             'Outlook not so good',
+             'Very doubtful']
+
+print('\n',messagens[random.randint(0, len(messagens) - 1)],'\n')
+
+#Toda Honra e Toda Glória Ao Deus de Abraão, Isaac, Jacó, Israel e Moisés Ao Seu Espírito Santo E Ao Seu Filho Amado Jesus Cristo. Louvado Seja Nosso Senhor Jesus Cristo, Para Sempre Seja Louvado. Amém.
