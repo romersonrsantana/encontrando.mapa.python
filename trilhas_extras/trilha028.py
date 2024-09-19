46d64b57d26f842171d266716292dcde2c932598
romersonrsantana <romersonsantana@hotmail.com.br>
2024-09-19 18:46:48 -0300

    de passo em passo
    Sagrado Coração de Jesus confio e espero em Vós. Amém.

---
+++ trilhas_extras/trilha028.py
@@ -0,0 +1,28 @@
+print('==='*27)
+print(' '*27,'Grade imagem')
+print('==='*27)
+
+def control(conta):
+    for c in grid:
+        print(c[contador], end='')
+
+grid = [['.','.','.','.','.','.'],
+        ['.','O','O','.','.','.'],
+        ['O','O','O','O','.','.'],
+        ['O','O','O','O','O','.'],
+        ['.','O','O','O','O','O'],
+        ['O','O','O','O','O','.'],
+        ['O','O','O','O','.','.'],
+        ['.','O','O','.','.','.'],
+        ['.','.','.','.','.','.']
+        ]
+
+contador = 0
+
+while contador < 6:
+    control(contador)
+    print('\n')
+    contador += 1
+print('\nObrigado Senhor S2 :)\n')
+
+#te Amo Papai do Céu, Deus de Abraão, Isaac, Jacó, Israel e Moisés, te Amo Espírito Santo, te Amo Jesus Cristo. Louvado Seja Nosso Senhor Jesus Cristo, Para Sempre Seja Louvado. Amém. 
