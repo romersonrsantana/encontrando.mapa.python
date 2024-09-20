ecd43d2eaa1132efca0bc02a7c6a4df56ac60cc8
romersonrsantana <romersonsantana@hotmail.com.br>
2024-09-19 21:21:01 -0300

    de passo em passo
    Sagrado Coração de Jesus Confio e Espero em Vós. Amém.

---
+++ aulas/aula017/aula017.py
@@ -0,0 +1,20 @@
+lista = (1, 3, 7, 16)
+print(lista)
+
+#lista[2] = 28
+
+listaII = [1, 3, 7, 16]
+print(listaII)
+
+listaII[1] = 28
+
+print(listaII)
+
+valores = []
+
+for c in range(0, 5):
+    valores.append(int(input('\nDigite um valor --> ')))
+
+print(f'\nthe value are: {valores}\n')
+
+#Toda Honra e Toda Glória Ao Deus de Abraão, Isaac, Jacó, Israel e Moisés Ao Seu Espírito Santos e Ao Seu Filho Amado Jesus Cristo. Louvado Seja Nosso Senhor Jesus Cristo, Para Sempre Seja Louvado. Amém.
