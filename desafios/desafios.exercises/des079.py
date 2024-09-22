ccef4dab1d9c0178266b0bc73a5613ef7b0b10a8
romersonrsantana <romersonsantana@hotmail.com.br>
2024-09-22 19:48:28 -0300

    de passo em passo
    Sagrado Coração de Jesus, Confio e Espero em Vós. Amém.

--- desafios/desafios.exercises/des079.py
+++ desafios/desafios.exercises/des079.py
@@ -2,4 +2,39 @@ print('---'*27)
 print(' '*27, 'Database')
 print('---'*27)
 
+number = control = choise = 0
+numbers_list = []
+boolean = False
+
+while boolean != True:
+
+    try:
+        number = int(input('Enter a number: --> '))
+    except:
+        print('Enter a value number!')
+        continue
+
+    if number in numbers_list:
+        print('Value not accepted!')
+    else:
+        numbers_list.append(number)
+    
+    choise = str(input('Do you want to continue?(Y-yes / N-not)')).lower()[0]
+
+    while True:
+        if choise == 'n':
+            print('Program closed!')
+            boolean = True
+            break
+        elif choise == 'y':
+            break
+        else:
+            print("Enter 'y' or 'n' ")
+
+print(numbers_list)
+    
+    
+
+
+
 #te Amo Papai do Céu Deus de Abraão, Isaac, Jacó, Israel e Moisés, te Amo Espírito Santo, te Amo Jesus Cristo. Louvado Seja Nosso Senhor Jesus Cristo, Para Sempre Seja Louvado. Amém.
