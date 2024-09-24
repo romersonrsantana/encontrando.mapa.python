4503083547c2380abee10c2ce2c4153e41f14a39
romersonrsantana <romersonsantana@hotmail.com.br>
2024-09-24 14:17:43 -0300

    de passo em passo
    Sagrado Coração de Jesus Confio e Espero em Vós. Amém.

---
+++ trilhas_extras/trilha031.py
@@ -0,0 +1,23 @@
+print()
+print('&&&'*27)
+print(' '*27,'Variáveis Multiplas')
+print('&&&'*27)
+
+spam = {'Name':'João', ' Age': '55',
+        'Name1':'Tereza',' Age1': '56'}
+
+for c, d in spam.items():
+    print(' Key: ' + c + ' value: ' + str(d))
+
+print()
+print('==='*27)
+
+picnic_items = {'apples': 5, 'cups': 2}
+
+print('I am bringing ' + str(picnic_items.get('cups', 0)) + ' cups.')
+
+print('I am bringing ' + str(picnic_items.get('eggs', 0)) + ' eggs.')
+
+
+
+#Toda Honra e Toda Glória Ao Papai do Céu Deus de Abraão, Isaac, Jacó, Israel e Moisés e Ao Seu Filho Amado Jesus Cristo. Louvado Seja Nosso Senhor Jesus Cristo, Para Sempre Seja Louvado. Amém.
---
+++ trilhas_extras/trilha032.py
@@ -0,0 +1,20 @@
+print('§§§'*27)
+print(' '*36, 'Test')
+print('§§§'*27)
+
+spam = {'name':'Tereza', 'age':'55'}
+
+if 'nickname' not in spam:
+    spam['nickname'] = 'bebê'
+
+print('\n',spam,'\n')
+
+spam.setdefault('color','purple')
+
+print('\n',spam,'\n')
+
+spam.setdefault('color', 'pink')
+
+print(spam) #retorna o valor existente na chave color.
+
+#Te Amo Pai Celestial Papai do Céu Deus de Abraão, Isaac, Jacó, Israel e Moisés. Amo o Espírito Santo e Ao Seu Filho Amado Jesus Cristo. Louvado Seja Nosso Senhor Jesus Cristo, Para Sempre Seja Louvado. Amém.
