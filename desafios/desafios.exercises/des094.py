29bc2baf49e2a892ea2103e04cafd2e4c9cf7a16
romersonrsantana <romersonsantana@hotmail.com.br>
2024-11-05 21:03:59 -0300

    de passo em passo
    Sagrado Coração de Jesus Confio e Espero em Vós. Louvado Seja Nosso Senhor Jesus Cristo, Para Sempre Seja Louvado. Amém.

--- desafios/desafios.exercises/des094.py
+++ desafios/desafios.exercises/des094.py
@@ -1,1 +1,30 @@
+print('---'*27)
+print(' '*36, 'Data history')
+print('---'*27)
+
+people = dict()
+person = dict()
+people_list = list()
+control = 1
+
+while True:
+    print()
+    person[f'name {control}'] = str(input('>>> Enter a Name: '))
+    print()
+    person[f'sexo {control}'] = str(input('>>> Write the sexo (M - masculino or F - feminino) '))
+    print()
+    person[f'age {control}'] = int(input(f">>> Enter the {person[f'name {control}']}'s age "))
+
+    control += 1
+
+    people_list.append(person.copy())
+    person.clear()
+
+    choise = str(input('>>> Do you to continue? (y - yes or n - not) ')).lower()[0]
+
+    if choise == 'n':
+        break
+
+print(people_list)
+
 #Toda Honra e Toda Glória Ao Deus de Abraão, Isaac, Jacó, Israel e Moisés E Ao Seu Filho Amado Jesus Cristo. Louvado Seja Nosso Senhor Jesus Cristo, Para Sempre Seja Louvado. Amém.
