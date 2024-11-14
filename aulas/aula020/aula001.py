7761ec63dcf64f4f1cce3c3dfe0bd3fd1e768741
romersonrsantana <romersonsantana@hotmail.com.br>
2024-11-14 18:40:54 -0300

    de passo em passo
    Sagrado Coração de Jesus Confio e Espero em Vós. Louvado Seja Nosso Senhor Jesus Cristo, Para Sempre Seja Louvado. Amém.

---
+++ aulas/aula020/aula001.py
@@ -0,0 +1,11 @@
+def mensage(msg):
+    print('---'*27)
+    print(' '*36,msg)
+    print('---'*27)
+
+
+mensage('Study')
+mensage('Church')
+mensage('Bible')
+
+#Toda Honra e Toda Glória Ao Deus de Abraão, Isaac, Jacó, Israel e Moisés E Ao Seu Filho Amado Jesus Cristo. Louvado Seja Nosso Senhor Jesus Cristo, Para Sempre Seja Louvado. Amém.
---
+++ aulas/aula020/aula002.py
@@ -0,0 +1,15 @@
+def layout(msg):
+    print()
+    print('---'*27)
+    print(' '*36,msg)
+    print('---'*27)
+    print()
+
+
+def sum(a, b):
+    return a + b
+
+
+layout(sum(2,7))
+
+#Toda Honra e Toda Glória Ao Deus de Abraão, Isaac, Jacó, Israel e Moisés E Ao Seu Filho Amado Jesus Cristo. Louvado Seja Nosso Senhor Jesus Cristo, Para Sempre Seja Louvado. Amém.
