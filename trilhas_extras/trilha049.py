832747adcd3164fc7c635ebaaac5e8369a55c11b
romersonrsantana <romersonsantana@hotmail.com.br>
2024-12-04 14:05:12 -0300

    de passo em passo
    Sagrado Coração de Jesus Confio e Espero em Vós. Louvado Seja Nosso Senhor Jesus Cristo. Para Sempre Seja Louvado. Amém.

---
+++ trilhas_extras/trilha049.py
@@ -0,0 +1,37 @@
+import re
+
+def layout(msg):
+    print('==='*27)
+    print(' '*36,msg)
+    print('==='*27)
+
+
+def scan(resp):
+    print()
+    print(f' >>> {resp}')
+    print()
+
+layout('Opcional match using (?) ')
+
+bat_regex = re.compile(r'Bat(wo)?man')
+mo1 = bat_regex.search('The adventures of Batman and Batwoman')
+
+scan(mo1.group()) #A preferência é Batwoman, caso não ouvesse seria imprimido Batman.
+
+layout('Example with area code')
+phone_regex = re.compile(r'(\d\d\d-)?(\d\d\d-\d\d\d\d)')
+mo2 = phone_regex.search('My number is 555-7172')
+scan(mo2.group())
+
+layout('Occurences using asteridco')
+
+bat_regex = re.compile(r'(Bat)*woman') #Número indeterminado de ocorrências
+mo3 = bat_regex.search('The adventures of BatBatwomanwonwowowoman')
+
+if mo3 == None:
+    scan('Value not found!')
+else:
+    scan(mo3.group())
+
+
+#Toda Honra e Toda Glória Ao Deus de Abraão, Isaac, Jacó, Israel e Moisés E Toda Honra e Toda Glória Ao Seu Filho Amado Jesus Cristo. Louvado Seja Nosso Senhor Jesus Cristo, Para Sempre Seja Louvado. Amém.
