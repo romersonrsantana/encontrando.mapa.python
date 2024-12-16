c381296acc5b648e148e5e5fcdebcda127bda3b9
romersonrsantana <romersonsantana@hotmail.com.br>
2024-12-16 07:32:08 -0300

    de passo em passo
    Sagrado Coração de Jesus Confio e Espero em Vós. Louvado Seja Nosso Senhor Jesus Cristo, Para Sempre Seja Louvado. Amém.

--- desafios/desafios.exercises/des096.py
+++ desafios/desafios.exercises/des096.py
@@ -1,7 +1,21 @@
-print('==='*27)
-print(' '*36,'Area')
-print('==='*27)
+def layout(caracterie, msg):  
+    print(f'{caracterie}'*27)
+    print(' '*36,msg)
+    print(f'{caracterie}'*27)
 
+
+def area(width, height):
+    return width * height
+
+
+layout('===', 'Rectangular area')
+
+width = float(input('>>> Enter a value to width (m): '))
+height = float(input('>>> Enter a value to height (m): '))
+
+total = area(width, height)
+
+layout('---', f'The area has {total:.2f} m².')
 
 
 #Toda Honra e Toda Glória Ao Deus de Abraão, Isaac, Jacó, Israel e Moisés e Ao Seu Filho Amado Jesus Cristo. Louvado Seja Nosso Senhor Jesus Cristo, Para Sempre Seja Louvado. Amém.
