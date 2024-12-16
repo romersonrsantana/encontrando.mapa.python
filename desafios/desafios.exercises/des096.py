91e2e7cf17ad185217b926e871aaee7600f43d3f
romersonrsantana <romersonsantana@hotmail.com.br>
2024-12-16 09:24:39 -0300

    de passo em passo
    Sagrado Coração de Jesus, Confio e Espero em Vós. Louvado Seja Nosso Senhor Jesus Cristo, Para Sempre Sempre Louvado. Amém.

--- desafios/desafios.exercises/des096.py
+++ desafios/desafios.exercises/des096.py
@@ -4,18 +4,16 @@ def layout(caracterie, msg):
     print(f'{caracterie}'*27)
 
 
-def area(width, height):
-    return width * height
+def area(width, height):    
+    total = width * height
+    layout('---', f'The area has {total:.2f} m².')
 
 
 layout('===', 'Rectangular area')
 
-width = float(input('>>> Enter a value to width (m): '))
-height = float(input('>>> Enter a value to height (m): '))
+wid = float(input('>>> Enter a value to width (m): '))
+hei = float(input('>>> Enter a value to height (m): '))
 
-total = area(width, height)
-
-layout('---', f'The area has {total:.2f} m².')
-
+area(wid, hei)
 
 #Toda Honra e Toda Glória Ao Deus de Abraão, Isaac, Jacó, Israel e Moisés e Ao Seu Filho Amado Jesus Cristo. Louvado Seja Nosso Senhor Jesus Cristo, Para Sempre Seja Louvado. Amém.
--- desafios/desafios.exercises/des097.py
+++ desafios/desafios.exercises/des097.py
@@ -1,1 +1,18 @@
+def layout(caractere, title):
+    print(f'{caractere}'*36)
+    print(' '*43,f'{title}')
+    print(f'{caractere}'*36)
+
+def write(mensage, items):
+    print(f'{items}')
+    print(f'   {mensage}')
+    print(f'{items}')
+
+
+mens = str(input('Write a mensage: >>> '))
+i = '-' * (len(mens) + 6)  
+layout('===', 'Responsive')
+write(mens, i)
+
+
 #Toda Honra e Toda Glória Ao Deus de Abraão, Isaac, Jacó, Israel e Moisés E Toda Honra e Toda Glória Ao Seu Filho Amado Jesus Cristo. Louvado Seja Nosso Senhor Jesus Cristo, Para Sempre Seja Louvado. Amém.
