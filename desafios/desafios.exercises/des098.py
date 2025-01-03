d1b220a3fec402e5f87d5f9d2affcee014aeba4e
romersonrsantana <romersonsantana@hotmail.com.br>
2025-01-02 21:18:00 -0300

    de passo em passo
    Sagrado Coração de Jesus Confio e Espero em Vós. Louvado Seja Nosso Senhor Jesus Cristo, Para Sempre Seja Louvado. Amém.

--- desafios/desafios.exercises/des098.py
+++ desafios/desafios.exercises/des098.py
@@ -16,12 +16,32 @@ print()
 while True:
     print('''
           Choice:
-                    A) Numbers from 1 to 10 of one and one. 
-                    B) Numbers from 10 to 0 of two and two.
-                    C) Enter the values.
+                    A) Numbers from 1 to 12 of one and one. 
+                    B) Numbers from 12 to 0 of two and two.
+                    C) Enter the value.
+                    D) Exit.
           
           ''')
     
-    choice = str(input('Inform the option: >>> '))
+    choice = str(input('Inform the option: >>> ')).lower()[0]
+
+    if choice == 'a':
+        parameter(1, 12, 1)
+    elif choice == 'b':
+        parameter(12, 0-1, -1)
+    elif choice == 'c':
+        one = int(input('       Enter a number to start: >>> '))
+        two = int(input('''       
+                        Enter a number to finalize: 
+            (For countdown enter the highest value followed by the stop value with the step(arithmic radio) 
+            **must be a negative number) 
+                        >>> '''))
+        three = int(input('     Enter a number to step: >>> ')  )
+        parameter(one, two, three)
+    elif choice == 'd':
+        break
+    else:
+        print()
+        print('     [Alert] Please provide correct option.')
 
 #Toda Honra e Toda Glória Ao Deus de Abraão, Isaac, Jacó, Israel e Moisés e Toda Honra e Toda Glória Ao Seu Filho Amado Jesus Cristo. Sagrado Coração de Jesus Confio e Espero em Vós. Louvado Seja Nosso Senhor Jesus Cristo, Para Sempre Seja Louvado. Amém.
