3a05c711a1273d3efb89ead57bd191ea756c51ad
romersonrsantana <romersonsantana@hotmail.com.br>
2025-01-08 19:33:32 -0300

    de passo em passo
    Sagrado Coração de Jesus Confio e Espero em Vós. Louvado Seja Nosso Senhor Jesus Cristo, Para Sempre Seja Louvado. Amém.

--- desafios/desafios.exercises/des099.py
+++ desafios/desafios.exercises/des099.py
@@ -1,3 +1,8 @@
+def layout(n, mensage):
+    print(f'{n}'*36)
+    print(' '*36, f'{mensage}')
+    print(f'{n}'*36)
 
+layout('===', 'Te Amo Mamãe Tereza')
 
 #Toda Honra e Toda Glória Ao Deus de Abraão, Isaac, Jacó, Israel e Moisés e Toda Honra e Toda Glória Ao Seu Filho Amado Jesus Cristo. Louvado Seja Nosso Senhor Jesus Cristo, Para Sempre Seja Louvado. Amém.
