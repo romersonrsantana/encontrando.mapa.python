7964c478e4e36e91c5a67edf5c6cad4c1b318882
romersonrsantana <romersonsantana@hotmail.com.br>
2025-01-10 08:31:13 -0300

    de passo em passo
    Sagrado Coração de Jesus Confio e Espero em Vós, Louvado Seja Nosso Senhor Jesus Cristo, Para Sempre Seja Louvado. Amém.

--- desafios/desafios.exercises/des099.py
+++ desafios/desafios.exercises/des099.py
@@ -3,6 +3,33 @@ def layout(n, mensage):
     print(' '*36, f'{mensage}')
     print(f'{n}'*36)
 
+def bigger(*number):
+    for c, v in enumerate(number):
+        if c == 0:
+            big_number = v
+        else:
+            if v > big_number:
+                big_number = v
+    print(f'The big number was {big_number}')
+
 layout('===', 'Te Amo Mamãe Tereza')
+
+layout('===', 'Bigger function')
+
+bigger(5, 9, 16, 36, 28)
+
+bigger(16, 28, 6, 7, 2, 9)
+
+number_val = list()
+
+while True:
+
+    n = int(input('Enter a name or (enter 0 to stop) >>> '))
+
+    if n == 0:
+        bigger(*number_val)
+        break
+    else:
+        number_val.append(n)
 
 #Toda Honra e Toda Glória Ao Deus de Abraão, Isaac, Jacó, Israel e Moisés e Toda Honra e Toda Glória Ao Seu Filho Amado Jesus Cristo. Louvado Seja Nosso Senhor Jesus Cristo, Para Sempre Seja Louvado. Amém.
