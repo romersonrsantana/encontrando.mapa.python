427d3c2efffe0dc50c91b0cb8d1afc47b3b87adc
romersonrsantana <romersonsantana@hotmail.com.br>
2025-01-15 08:51:54 -0300

    de passo em passo
    Sagrado Coração de Jesus Confio e Espero em Vós. Louvado Seja Nosso Senhor Jesus Cristo, Para Sempre Seja Louvado. Amém.

--- desafios/desafios.exercises/des101.py
+++ desafios/desafios.exercises/des101.py
@@ -10,15 +10,29 @@ def vote(year_of_birth):
     years_old = current_year - year_of_birth
 
     if years_old >= 0 and years_old < 16:
-        return "Vote denied"
+        return f"You are {years_old} years old.Vote denied."
     elif years_old == 16 or years_old == 17 or years_old > 70 and years_old < 112:
-        return "Opitional vote"
+        return f"You are {years_old} years old. Opitional vote."
     elif years_old >= 18 and years_old < 70:
-        return "Mandatory vote"
+        return f"You are {years_old} years old. Mandatory vote."
 
 current_year = date.today().year
 
 layout('===', 'Right to vote')
 
+while True:
+    user = int(input('>>> Enter your year of birth --> '))
+
+    if user > current_year:
+        print('[ERRO] Year not accepted')
+    elif user <= current_year and user > 1913:
+        break
+    else:
+        print('[ERRO] Year not accepted, vampire!')
+
+response = vote(user)
+print()
+print(f'        >>> As for your vote this year, {response} ')
+print()
 
 #Toda Honra e Toda Glória Ao Deus de Abraão, Isaac, Jacó, Israel e Moisés E Ao Seu Único Filho Amado Jesus Cristo. Louvado Seja Nosso Senhor Jesus Cristo, Para Sempre Seja Louvado. Amém.
