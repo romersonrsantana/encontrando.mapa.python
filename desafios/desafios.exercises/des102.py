207c6de2c711c721c3b12473e4e9cd525038be49
romersonrsantana <romersonsantana@hotmail.com.br>
2025-01-16 10:21:18 -0300

    de passo em passo
    Sagrado Coração de Jesus Confio e Espero em Vós. Louvado Seja Nosso Senhor Jesus Cristo, Para Sempre Seja Louvado. Amém.

--- desafios/desafios.exercises/des102.py
+++ desafios/desafios.exercises/des102.py
@@ -1,3 +1,28 @@
+def layout(caracter, msg):
+    print(f'{caracter}'*36)
+    print(' '*36,f'{msg}')
+    print(f'{caracter}'*36)
 
+
+def factorial(number, show=0):
+    if show == 0:
+        factorial = 1
+        while number > 0:
+            factorial *= number
+            number -= 1
+        return factorial
+    else:
+        factorial = 1
+        while number > 1:
+            print(f'{number} ', end='-')
+            factorial *= number
+            number -= 1
+        print(f'{number}', end='')
+        print()
+        return factorial
+        
+test = factorial(5, 1)
+
+print(f'The factorial is {test}')
 
 #Toda Honra e Toda Glória Ao Deus de Abraão, Jacó, Israel e Moisés E Ao Seu Único Filho Jesus Cristo. Louvado Seja Nosso Senhor Jesus Cristo, Para Sempre Seja Louvado. Amém.
