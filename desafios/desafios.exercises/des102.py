07d696cea8005e6ab4f5e904c525de71362acbbc
romersonrsantana <romersonsantana@hotmail.com.br>
2025-01-16 21:36:39 -0300

    de passo em passo
    Sagrado Coração de Jesus Confio e Espero em Vós. Louvado Seja Nosso Senhor Jesus Cristo, Para Sempre Seja Louvado. Amém.

--- desafios/desafios.exercises/des102.py
+++ desafios/desafios.exercises/des102.py
@@ -14,15 +14,30 @@ def factorial(number, show=0):
     else:
         factorial = 1
         while number > 1:
-            print(f'{number} ', end='-')
+            print(f' {number} ', end='x')
             factorial *= number
             number -= 1
-        print(f'{number}', end='')
+        print(f' {number} ', end='')
         print()
         return factorial
-        
-test = factorial(5, 1)
+    
 
-print(f'The factorial is {test}')
+print()
+num_factored = int(input('>>> Enter a number to be factored: '))
 
+while True:
+    print()   
+    choise = int(input('>>> Report 0 to check the  total or 1 to check the factorial calculation: '))
+
+    if choise == 0 or choise == 1:
+        break
+    else:
+        print('[Alert] Report 1 or 2!')
+
+layout('===', f'>>> The factorial of the number {num_factored} is: ')
+print()
+test = factorial(num_factored, choise)
+print()
+print(f'>>> {num_factored}! = {test} .')
+print()
 #Toda Honra e Toda Glória Ao Deus de Abraão, Jacó, Israel e Moisés E Ao Seu Único Filho Jesus Cristo. Louvado Seja Nosso Senhor Jesus Cristo, Para Sempre Seja Louvado. Amém.
