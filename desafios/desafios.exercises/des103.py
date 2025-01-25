65c0882cdf83a58e485da2f6a9f16a5520e28983
romersonrsantana <romersonsantana@hotmail.com.br>
2025-01-24 21:10:44 -0300

    de passo em passo
    Não é pela minha força mas pela Sua Autoridade Jesus. Sagrado Coração de Jesus, Confio e Espero em Vós. Louvado Seja Nosso Senhor Jesus Cristo, Pra Sempre Seja Louvado. Amém.

--- desafios/desafios.exercises/des103.py
+++ desafios/desafios.exercises/des103.py
@@ -1,3 +1,18 @@
+def layout(caracter, msg):
+    print(f'{caracter}'*36)
+    print(' '*36, f'{msg}')
+    print(f'{caracter}'*36)
 
+
+def record(name = 0, goals_total = 0):
+    if name != 0 or goals_total != 0:
+        for k, v in football_players.items():
+            if name == k or goals_total == v:
+                print(f'>>> {k} : {v}')
+
+
+football_players = {'João': 26 , 'Tereza': 27 }
+
+layout('===', 'Checker')
 
 #Toda Honra e Toda Glória Ao Deus de Abraão, Isaac, Jacó, Israel e Moisés E Toda Honra e Toda Glória Ao Seu Filho Amado Jesus Cristo. Louvado Seja Nosso Senhor Jesus Cristo, Para Sempre Seja Louvado. Amém.
