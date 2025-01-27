d68088cfb6cdfcfe3c354da608db85112cf44ab0
romersonrsantana <romersonsantana@hotmail.com.br>
2025-01-27 07:54:49 -0300

    de passo em passo
    Sagrado Coração de Jesus Confio e Espero em Vós, que jamais seja pela minha força, mas pela Sua Autoridade Jesus Cristo. Louvado Seja Nosso Senhor Jesus Cristo, Para Sempre Seja Louvado. Amém.

--- desafios/desafios.exercises/des103.py
+++ desafios/desafios.exercises/des103.py
@@ -8,11 +8,35 @@ def record(name = 0, goals_total = 0):
     if name != 0 or goals_total != 0:
         for k, v in football_players.items():
             if name == k or goals_total == v:
-                print(f'>>> {k} : {v}')
+                print(f'>>> Name: {k} : {v} goals.')
+    else:
+        print()
+        print('     [Alert] Informe os dados...')
+        print()
+
+def name():
+    return str(input('>>> Enter a name player: '))
+
+
+def goals():
+    while True:
+        try:
+            return int(input('>>> Enter a goals numbers: '))
+        except ValueError:
+            print('\n [Error] Enter an integer. ')
+            print()
 
 
 football_players = {'João': 26 , 'Tereza': 27 }
 
-layout('===', 'Checker')
+
+def main():
+    layout('===', 'Checker')
+    record(name(), goals())
+
+
+if __name__ == '__main__':
+    main()
+
 
 #Toda Honra e Toda Glória Ao Deus de Abraão, Isaac, Jacó, Israel e Moisés E Toda Honra e Toda Glória Ao Seu Filho Amado Jesus Cristo. Louvado Seja Nosso Senhor Jesus Cristo, Para Sempre Seja Louvado. Amém.
